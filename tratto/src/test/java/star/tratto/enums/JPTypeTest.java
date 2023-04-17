62134fab08741053c10abbca0821d255b9a580d2
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-04-17 11:49:17 -0700

    added tests for enums package.

---
+++ tratto/src/test/java/star/tratto/enums/JPTypeTest.java
@@ -0,0 +1,35 @@
+package star.tratto.enums;
+
+import com.github.javaparser.ast.type.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class JPTypeTest {
+    @Test
+    public void testGetValue() {
+        assertEquals("ARRAY_TYPE", JPType.ARRAY_TYPE.getValue());
+        assertEquals("CLASS_OR_INTERFACE_TYPE", JPType.CLASS_OR_INTERFACE_TYPE.getValue());
+        assertEquals("INTERSECTION_TYPE", JPType.INTERSECTION_TYPE.getValue());
+        assertEquals("PRIMITIVE_TYPE", JPType.PRIMITIVE_TYPE.getValue());
+        assertEquals("REFERENCE_TYPE", JPType.REFERENCE_TYPE.getValue());
+        assertEquals("TYPE_PARAMETER", JPType.TYPE_PARAMETER.getValue());
+        assertEquals("UNION_TYPE", JPType.UNION_TYPE.getValue());
+        assertEquals("UNKNOWN_TYPE", JPType.UNKNOWN_TYPE.getValue());
+        assertEquals("VAR_TYPE", JPType.VAR_TYPE.getValue());
+        assertEquals("VOID_TYPE", JPType.VOID_TYPE.getValue());
+        assertEquals("WILDCARD", JPType.WILDCARD.getValue());
+    }
+
+    @Test
+    public void testGetJPTypeDeclaration() {
+        assertEquals("CLASS_OR_INTERFACE_TYPE", JPType.getJPTypeDeclaration(new ClassOrInterfaceType()).getValue());
+        assertEquals("PRIMITIVE_TYPE", JPType.getJPTypeDeclaration(new PrimitiveType()).getValue());
+        assertEquals("TYPE_PARAMETER", JPType.getJPTypeDeclaration(new TypeParameter()).getValue());
+        assertEquals("UNION_TYPE", JPType.getJPTypeDeclaration(new UnionType()).getValue());
+        assertEquals("UNKNOWN_TYPE", JPType.getJPTypeDeclaration(new UnknownType()).getValue());
+        assertEquals("VAR_TYPE", JPType.getJPTypeDeclaration(new VarType()).getValue());
+        assertEquals("VOID_TYPE", JPType.getJPTypeDeclaration(new VoidType()).getValue());
+        assertEquals("WILDCARD", JPType.getJPTypeDeclaration(new WildcardType()).getValue());
+    }
+}
---
+++ tratto/src/test/java/star/tratto/enums/PathTest.java
@@ -0,0 +1,16 @@
+package star.tratto.enums;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PathTest {
+    @Test
+    public void testPath() {
+        String userDir = System.getProperty("user.dir");
+        assertEquals(userDir + "/src/main/java/data/collection/javaparser", Path.JAVA_PARSER.getValue());
+        assertEquals(userDir + "/target/output", Path.OUTPUT.getValue());
+        assertEquals(userDir + "/src/main/resources", Path.RESOURCES.getValue());
+        assertEquals(userDir + "/src/main/repos", Path.REPOS.getValue());
+    }
+}
