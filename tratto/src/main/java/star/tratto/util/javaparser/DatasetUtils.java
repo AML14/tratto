4b3e14518b4ece7c40d96e7ec87bf93fee0f2c47
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-05-02 19:37:28 -0700

    linted DatasetUtils class for current functionality

--- tratto/src/main/java/star/tratto/util/javaparser/DatasetUtils.java
+++ tratto/src/main/java/star/tratto/util/javaparser/DatasetUtils.java
@@ -4,23 +4,12 @@ import com.github.javaparser.ast.CompilationUnit;
 import star.tratto.dataset.oracles.JDoctorCondition.*;
 import star.tratto.exceptions.PrimaryTypeNotFoundException;
 import star.tratto.identifiers.file.*;
-import star.tratto.identifiers.JavadocValueType;
 import static star.tratto.util.javaparser.JavaParserUtils.*;
-
-import org.javatuples.Pair;
 
 import java.nio.file.Paths;
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class DatasetUtils {
-
-    private <T> List<T> removeDuplicates(List<T> list) {
-        Set<T> set = new LinkedHashSet<>(list);
-        return new ArrayList<>(set);
-    }
-
     public static String getSourceCode(
             Operation operation,
             String sourcePath
