1808c2e8c10c1d4cab3b96c424359eeb9f372a4f
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-05-09 20:42:33 -0700

    updated JDoctorConditionParser to use ArrayLists rather than simple arrays due to switch from records.

--- tratto/src/main/java/star/tratto/dataset/oracles/json/parsers/JDoctorConditionParser.java
+++ tratto/src/main/java/star/tratto/dataset/oracles/json/parsers/JDoctorConditionParser.java
@@ -7,6 +7,10 @@ import star.tratto.dataset.oracles.Project;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * A helper class that deserialize the JDoctor conditions in JSON format, generating the corresponding Java record
@@ -14,24 +18,29 @@ import java.util.Arrays;
  */
 public class JDoctorConditionParser {
     /**
-     * Given a Java project the method reads all the JSON files containing JDoctor conditions in JSON format,
-     * and returns an array of {@link JDoctorCondition} objects, representing the corresponding Java records of all the
-     * JDoctor conditions of the project.
+     * Given a Java project the method reads all the JSON files containing
+     * JDoctor conditions in JSON format, and returns an array of
+     * {@link JDoctorCondition} objects, representing the corresponding Java
+     * records of all the JDoctor conditions of the project.
      *
-     * @param project The Java project associated to the JDoctor conditions to parse.
-     * @return An array of {@link JDoctorCondition} objects, representing the Java record representations of the original
-     * JDoctor conditions in the JSON format
+     * @param project The Java project associated to the JDoctor conditions
+     *                to parse.
+     * @return An array of {@link JDoctorCondition} objects, representing the
+     * Java record representations of the original JDoctor conditions in the
+     * JSON format.
      */
-    public JDoctorCondition[] parseJDoctorConditions(Project project) {
-        File jDocConditionsDir = new File(project.getjDoctorConditionsPath());
-        File[] jDocConditionsFiles = jDocConditionsDir.listFiles();
-
-        JDoctorCondition[] jDocConditions = Arrays.stream(jDocConditionsFiles)
-                .filter(jDocConditionFile -> jDocConditionFile.getName().endsWith(".json"))
-                .map(jDocConditionFile -> json2java(jDocConditionFile))
-                .flatMap(Arrays::stream)
-                .toArray(JDoctorCondition[]::new);
-        return jDocConditions;
+    public List<JDoctorCondition> parseJDoctorConditions(Project project) {
+        // get path of JDoctor conditions.
+        File jDoctorConditionsDir = new File(project.getjDoctorConditionsPath());
+        // get all JSON files related to JDoctor conditions.
+        File[] jDoctorConditionsFiles = jDoctorConditionsDir.listFiles();
+        // convert each JSON file to list of JDoctorConditions.
+        if (jDoctorConditionsFiles == null) return new ArrayList<>();
+        return Arrays.stream(jDoctorConditionsFiles)
+                .filter(jDoctorConditionsFile -> jDoctorConditionsFile.getName().endsWith(".json"))
+                .map(this::json2java)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
     }
 
     /**
@@ -42,17 +51,17 @@ public class JDoctorConditionParser {
      * @return An array of {@link JDoctorCondition} representing the Java record representations of the original JDoctor
      * conditions in the JSON format, of the file passed to the function.
      */
-    private JDoctorCondition[] json2java(File jDoctorConditionsFile) {
+    private List<JDoctorCondition> json2java(File jDoctorConditionsFile) {
         ObjectMapper objectMapper = new ObjectMapper();
         try {
             JDoctorCondition[] conditions = objectMapper.readValue(
                     jDoctorConditionsFile,
                     JDoctorCondition[].class
             );
-            return conditions;
+            return Arrays.asList(conditions);
         } catch (IOException e) {
             e.printStackTrace();
-            return new JDoctorCondition[0];
+            return new ArrayList<>();
         }
     }
 }
