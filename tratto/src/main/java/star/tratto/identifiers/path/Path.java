3210c62830809e0c678f81bf868791e8aaaef7f6
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-05-09 20:41:42 -0700

    updated repos path to new repository layout

--- tratto/src/main/java/star/tratto/identifiers/path/Path.java
+++ tratto/src/main/java/star/tratto/identifiers/path/Path.java
@@ -6,11 +6,11 @@ public enum Path {
     JAVA_PARSER(Paths.get("src", "main", "java", "data", "collection", "javaparser").toString()),
     OUTPUT(Paths.get("target", "output").toString()),
     RESOURCES(Paths.get("src", "main", "resources").toString()),
-    REPOS(Paths.get("src", "main", "repos").toString());
+    REPOS(Paths.get("src", "main", "java", "star", "tratto", "dataset", "repos").toString());
 
     private final String path;
 
-    private Path(String path) {
+    Path(String path) {
         this.path = Paths.get(
                 System.getProperty("user.dir"),
                 path
