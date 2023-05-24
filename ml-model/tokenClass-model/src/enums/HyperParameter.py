b0902bf8fb4a6154aae57727f82807fbe727cb40
davide <davide.molinelli@mail.polimi.it>
2023-05-24 16:13:54 +0200

    modified batch size parameter

--- ml-model/tokenClass-model/src/enums/HyperParameter.py
+++ ml-model/tokenClass-model/src/enums/HyperParameter.py
@@ -5,7 +5,7 @@ class HyperParameter(Enum):
     Helper enums class: it defines the values of the hyperparameters of the model.
     """
     # number of sequence in each batch
-    BATCH_SIZE = 128
+    BATCH_SIZE = 8
     # learning rate for the gradient descent
     LR = 0.001
     # number of epochs to train the model
