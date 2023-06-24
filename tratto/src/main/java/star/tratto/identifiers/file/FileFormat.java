bf910d1735f0c0eafcb99c978f113d8fefb7b108
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-06-23 22:57:31 -0700

    added new FileUtils.

--- tratto/src/main/java/star/tratto/identifiers/file/FileFormat.java
+++ tratto/src/main/java/star/tratto/identifiers/file/FileFormat.java
@@ -3,7 +3,8 @@ package star.tratto.identifiers.file;
 public enum FileFormat {
     JAVA(".java"),
     JSON(".json"),
-    TXT(".txt");
+    TXT(".txt"),
+    CSV(".csv");
 
     private final String value;
 
--- tratto/src/main/java/star/tratto/util/FileUtils.java
+++ tratto/src/main/java/star/tratto/util/FileUtils.java
@@ -2,6 +2,8 @@ package star.tratto.util;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import star.tratto.exceptions.FileNotCreatedException;
+import star.tratto.exceptions.FolderCreationFailedException;
 import star.tratto.identifiers.file.FileFormat;
 import star.tratto.identifiers.file.FileName;
 
@@ -13,49 +15,73 @@ import java.util.Objects;
 
 
 public class FileUtils {
-
-    /**
-     * The method writes the content passed to the function to a file of a given format {@link FileFormat}. If the {@code append}
-     * parameter is {@code true} the method appends the content to the file (if it already exists), otherwise it create
-     * a new file and write the content within it.
-     *
-     * @param dirPath the path to the directory where the file must be saved
-     * @param fileName the name of the file where to write the content
-     * @param fileFormat the format of the file {@link FileFormat}
-     * @param content the content to write within the file
-     * @param append a boolean value. If true, the content is appended to the file (if it exists). Otherwise, the
-     *               method creates a new file.
-     * @throws IOException If the file cannot be written
-     */
-    public static void write(
+    public static void appendToFile(
             String dirPath,
             String fileName,
             FileFormat fileFormat,
-            Object content,
-            Boolean append
-    ) throws IOException {
-        // create a new file
-        File file = createFile(dirPath, fileName, fileFormat);
-        // depending on the file format, save the content, accordingly
-        switch (fileFormat) {
-            case TXT -> {
-                // Convert content to string
-                String fileContent = (String) content;
-                // Create a FileWriter object with append flag set to true
-                FileWriter writer = new FileWriter(file, append);
-                // Write content to the file
-                writer.write(fileContent + "\n");
-                // Close the FileWriter object
-                writer.close();
+            String projectName,
+            Object content
+    ) {
+        try {
+            // create a new file
+            File file = createFile(Paths.get(dirPath, projectName).toString(), fileName, fileFormat);
+            // depending on the file format, save the content, accordingly
+            switch (fileFormat) {
+                case TXT, CSV -> {
+                    // Convert content to string
+                    String fileContent = (String) content;
+                    // Create a FileWriter object with append flag set to true
+                    FileWriter writer = new FileWriter(file, true);
+                    // Write content to the file
+                    writer.write(fileContent + "\n");
+                    // Close the FileWriter object
+                    writer.close();
+                }
+                case JSON -> {
+                    ObjectMapper objectMapper = new ObjectMapper();
+                    objectMapper.writerWithDefaultPrettyPrinter().writeValue(file, content);
+                }
+                default -> {
+                    String errMsg = String.format("File format %s not yet supported to save the content of a file.", fileFormat.getValue());
+                    System.err.println(errMsg);
+                }
             }
-            case JSON -> {
-                ObjectMapper objectMapper = new ObjectMapper();
-                objectMapper.writerWithDefaultPrettyPrinter().writeValue(file, content);
+        } catch (IOException | FolderCreationFailedException | FileNotCreatedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void appendToFileExclusive(
+            String dirPath,
+            String fileName,
+            FileFormat fileFormat,
+            String projectName,
+            String content
+    ) {
+        String filePath = Paths.get(dirPath, projectName, fileName + fileFormat.getValue()).toString();
+        File file = new File(filePath);
+        boolean found = false;
+        if (file.exists()) {
+            try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    if (line.contains(content)) {
+                        found = true;
+                        break;
+                    }
+                }
+            } catch (IOException e) {
+                System.err.println("Error reading file: " + e.getMessage());
             }
-            default -> {
-                String errMsg = String.format("File format %s not yet supported to save the content of a file.", fileFormat.getValue());
-                System.err.println(errMsg);
-            }
+        }
+        if (!found) {
+            appendToFile(
+                    dirPath,
+                    fileName,
+                    fileFormat,
+                    projectName,
+                    content
+            );
         }
     }
 
@@ -69,22 +95,26 @@ public class FileUtils {
      * @return the file created {@link File}
      * @throws IOException If the file cannot be created
      */
-    public static File createFile(String dirPath, String fileName, FileFormat fileFormat) throws IOException {
+    public static File createFile(String dirPath, String fileName, FileFormat fileFormat) throws FolderCreationFailedException, IOException, FileNotCreatedException {
         String filePath = Paths.get(dirPath, fileName + fileFormat.getValue()).toString();
         File dir = new File(dirPath);
         File file = new File(filePath);
+        // create directory.
         if (!dir.exists()) {
             boolean success = dir.mkdirs();
             if (!success) {
                 String errMsg = String.format("Unable to create folder %s", dirPath);
                 System.err.println(errMsg);
+                throw new FolderCreationFailedException();
             }
         }
+        // create file.
         if (!file.exists()) {
             boolean fileCreated = file.createNewFile();
             if (!fileCreated) {
                 String errMsg = String.format("Unable to create file %s to path %s.", fileName, dirPath);
                 System.err.println(errMsg);
+                throw new FileNotCreatedException();
             }
         }
         return file;
@@ -118,7 +148,7 @@ public class FileUtils {
      * @return the complete path to a file.
      */
     public static String getAbsolutePathToFile(String dirPath, FileName fileName, FileFormat fileFormat) {
-        return Paths.get(dirPath, fileName.getValue()).toString() + fileFormat.getValue();
+        return Paths.get(dirPath, fileName.getValue()) + fileFormat.getValue();
     }
 
     /**
@@ -147,9 +177,10 @@ public class FileUtils {
         }
         ObjectMapper objectMapper = new ObjectMapper();
         try {
-            List<?> tokenList = objectMapper.readValue(
+            return objectMapper.readValue(
                 jsonFile,
-                new TypeReference<List<?>>() {}
+                    new TypeReference<>() {
+                    }
             );
         } catch (IOException e) {
             String errMsg = String.format("Unexpected error in processing the JSON file %s.", filePath);
