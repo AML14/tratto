c4971b310a77647cb4bafb658a454b9041d6fc53
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-06-23 23:03:15 -0700

    added file mapping.

--- tratto/src/main/java/star/tratto/OraclesDataset.java
+++ tratto/src/main/java/star/tratto/OraclesDataset.java
@@ -11,11 +11,12 @@ import star.tratto.identifiers.file.FileName;
 import star.tratto.identifiers.path.Path;
 import star.tratto.util.FileUtils;
 
-import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class OraclesDataset {
+    public static final int chunkSize = 100;
 
     public static void main(String[] args) {
         // Specify the path to JSON file with the list of the input projects and
@@ -30,25 +31,47 @@ public class OraclesDataset {
             // get JDoctor conditions and load project to generator.
             List<JDoctorCondition> jDoctorConditions = jDoctorConditionParser.parseJDoctorConditions(project);
             oracleDPGenerator.loadProject(project, jDoctorConditions);
+            // get oracle data points.
             List<OracleDatapoint> oracleDPList = oracleDPGenerator.generate();
-            /* Additional code for writing data points to file.
-            try {
-                FileUtils.write(
-                        Paths.get(Path.OUTPUT.getValue(), project.getProjectName()).toString(),
+            List<String> oraclesString = oracleDPList.stream().map(OracleDatapoint::getOracle).toList();
+            // save oracles.
+            String oracleStringFileName = String.format(
+                    "oracle_list_%s", project.getProjectName()
+            );
+            FileUtils.appendToFile(
+                    Path.OUTPUT.getValue(),
+                    oracleStringFileName,
+                    FileFormat.JSON,
+                    project.getProjectName(),
+                    oraclesString
+            );
+            // save OracleDatapoint chunks.
+            List<List<OracleDatapoint>> oracleDPChunks = splitListIntoChunks(oracleDPList);
+            for (int i = 0; i < oracleDPChunks.size(); i++) {
+                List<OracleDatapoint> chunk = oracleDPChunks.get(i);
+                String fileName = String.format(
+                        "%s_%s_%d",
+                        FileName.ORACLE_DATAPOINTS.getValue(),
+                        project.getProjectName(),
+                        i
+                );
+                FileUtils.appendToFile(
+                        Paths.get(Path.OUTPUT.getValue(),"dataset").toString(),
                         fileName,
                         FileFormat.JSON,
-                        oracleDPList,
-                        true
+                        project.getProjectName(),
+                        chunk
                 );
-            } catch (IOException e) {
-                String errMsg = String.format(
-                    "Error while saving the data-points of project %s in JSON format.",
-                    project.getProjectName()
-                );
-                System.err.println(errMsg);
             }
-             */
-            break;
         }
+    }
+
+    private static <T> List<List<T>> splitListIntoChunks(List<T> list) {
+        List<List<T>> chunks = new ArrayList<>();
+        for (int i = 0; i < list.size(); i += chunkSize) {
+            int endIndex = Math.min(i + chunkSize, list.size());
+            chunks.add(list.subList(i, endIndex));
+        }
+        return chunks;
     }
 }
