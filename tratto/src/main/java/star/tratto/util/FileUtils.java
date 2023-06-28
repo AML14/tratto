cfa93b6c66781039fd7eab6554d7f98a2cf939db
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-06-27 20:52:03 -0700

    added FileUtils class JavaDoc.

--- tratto/src/main/java/star/tratto/util/FileUtils.java
+++ tratto/src/main/java/star/tratto/util/FileUtils.java
@@ -14,6 +14,9 @@ import java.util.List;
 import java.util.Objects;
 
 
+/**
+ * This class manages file I/O utilities.
+ */
 public class FileUtils {
     public static void appendToFile(
             String dirPath,
