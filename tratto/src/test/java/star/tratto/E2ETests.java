7d85c85117bd2745f1fc5c504137c7c33f4e0756
Alberto Martín López <alberto.martin95@gmail.com>
2023-07-03 10:55:53 +0200

    Refactored azure-pipelines.yml

--- tratto/src/test/java/star/tratto/E2ETests.java
+++ tratto/src/test/java/star/tratto/E2ETests.java
@@ -27,6 +27,7 @@ public class E2ETests {
      * </ul>
      */
     @Test
+//    @Disabled
     public void datasetsE2ETest() throws IOException {
         // TODO: Generate oracles dataset. For the moment, we assume it's under src/main/resources/oracles-dataset/
 
@@ -53,6 +54,7 @@ public class E2ETests {
      * token ";" is returned.
      */
     @Test
+//    @Disabled
     public void tokenSuggesterE2ETest() {
 //        List<String> stringOracles = readOraclesFromExternalFiles();
 //        TokenSuggesterTest.getNextLegalTokensAuxTest(stringOracles);
