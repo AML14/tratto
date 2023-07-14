b463f9ac9244111dea26aa39eb9aa90557b9ebb3
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-07-14 13:04:31 -0700

    added documentation for CommonPrimitiveType

--- tratto/src/main/java/star/tratto/identifiers/CommonPrimitiveType.java
+++ tratto/src/main/java/star/tratto/identifiers/CommonPrimitiveType.java
@@ -9,6 +9,46 @@ import java.util.stream.Collectors;
  * uses single-letter representations of primitive types. The source code
  * format, used by JavaParser, uses the string representation in source code
  * to describe primitive types.
+ * We provide the following conversion table:
+ * <table>
+ *     <caption>Primitive representation conversion table</caption>
+ *     <tr>
+ *          <th scope="col">Field Descriptor</th>
+ *          <th scope="col">Source Code</th>
+ *     </tr>
+ *     <tr>
+ *         <td>Z</td>
+ *         <td>boolean</td>
+ *     </tr>
+ *     <tr>
+ *         <td>B</td>
+ *         <td>byte</td>
+ *     </tr>
+ *     <tr>
+ *         <td>C</td>
+ *         <td>char</td>
+ *     </tr>
+ *     <tr>
+ *         <td>S</td>
+ *         <td>short</td>
+ *     </tr>
+ *     <tr>
+ *         <td>I</td>
+ *         <td>int</td>
+ *     </tr>
+ *     <tr>
+ *         <td>J</td>
+ *         <td>long</td>
+ *     </tr>
+ *     <tr>
+ *         <td>F</td>
+ *         <td>float</td>
+ *     </tr>
+ *     <tr>
+ *         <td>D</td>
+ *         <td>double</td>
+ *     </tr>
+ * </table>
  */
 public class CommonPrimitiveType {
     // base primitive types.
@@ -32,6 +72,10 @@ public class CommonPrimitiveType {
             doubleClass
     );
 
+    /**
+     * @param fieldDescriptor a field descriptor String representation of a primitive type
+     * @return the primitive Class corresponding to the field descriptor
+     */
     private static Class<?> fieldDescriptorToClass(String fieldDescriptor) {
         switch (fieldDescriptor) {
             case "Z" -> {
@@ -62,6 +106,10 @@ public class CommonPrimitiveType {
         throw new IllegalArgumentException("Unrecognized primitive field descriptor: " + fieldDescriptor);
     }
 
+    /**
+     * @param sourceCode a source code String representation of a primitive type
+     * @return the primitive Class corresponding to the source code
+     */
     private static Class<?> sourceCodeToClass(String sourceCode) {
         switch (sourceCode) {
             case "boolean" -> {
@@ -92,6 +140,10 @@ public class CommonPrimitiveType {
         throw new IllegalArgumentException("Unrecognized primitive source code: " + sourceCode);
     }
 
+    /**
+     * @param primitiveClass a Class representation of a primitive type
+     * @return the field descriptor String representation of {@code primitiveClass}
+     */
     private static String classToFieldDescriptor(Class<?> primitiveClass) {
         if (primitiveClass.equals(booleanClass)) {
             return "Z";
@@ -113,24 +165,54 @@ public class CommonPrimitiveType {
         throw new IllegalArgumentException("Unrecognized class: " + primitiveClass);
     }
 
+    /**
+     * @param primitiveClass a Class representation of a primitive type
+     * @return the source code String representation of {@code primitiveClass}
+     */
     private static String classToSourceCode(Class<?> primitiveClass) {
         return primitiveClass.getName();
     }
 
+    /**
+     * Converts a field descriptor String representation of a primitive type
+     * to a source code String representation. See class JavaDoc for full list
+     * of conversions.
+     *
+     * @param fieldDescriptor a field descriptor String representation of a
+     *                        primitive type
+     * @return the corresponding source code String representation
+     */
     public static String convertFieldDescriptorToSourceCode(String fieldDescriptor) {
         Class<?> primitiveClass = fieldDescriptorToClass(fieldDescriptor);
         return classToSourceCode(primitiveClass);
     }
 
+    /**
+     * Converts a source code String representation of a primitive type to a
+     * field descriptor String representation. See class JavaDoc for full list
+     * of conversions.
+     *
+     * @param sourceCode a source code String representation of a primitive
+     *                   type
+     * @return the corresponding field descriptor String representation
+     */
     public static String convertSourceCodeToFieldDescriptor(String sourceCode) {
         Class<?> primitiveClass = sourceCodeToClass(sourceCode);
         return classToFieldDescriptor(primitiveClass);
     }
 
+    /**
+     * @return field descriptor String representation of all primitive types
+     */
     public static List<String> getAllFieldDescriptors() {
         return primitiveList.stream().map(CommonPrimitiveType::classToFieldDescriptor).collect(Collectors.toList());
     }
 
+    /**
+     *
+     * @return a regex of all possible field descriptor String representations
+     * of a primitive type. Used to find field descriptors in a given string.
+     */
     public static String getAllFieldDescriptorsRegex() {
         return "[" + String.join("", getAllFieldDescriptors()) + "]";
     }
