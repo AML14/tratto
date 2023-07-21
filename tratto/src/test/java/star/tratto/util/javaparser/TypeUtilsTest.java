7933139967e25c157db0d79f4a40133a14796f00
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-07-21 13:11:24 -0700

    re-designing TypeUtils to fit all necessary use cases.

--- tratto/src/test/java/star/tratto/util/javaparser/TypeUtilsTest.java
+++ tratto/src/test/java/star/tratto/util/javaparser/TypeUtilsTest.java
@@ -14,49 +14,4 @@ public class TypeUtilsTest {
         assertEquals("Oversuperstition", TypeUtils.removeTypeArguments("Oversuperstition"));
         assertEquals("Foo", TypeUtils.removeTypeArguments("Foo<? super Collection<T>, T>"));
     }
-
-    @Test
-    public void getNameSegmentsTest() {
-        assertEquals(List.of("normal", "package", "setup", "for", "a", "Class"), TypeUtils.getNameSegments("normal.package.setup.for.a.Class"));
-        assertEquals(List.of("spicy", "example", "that", "uses", "Many", "Inner", "Classes"), TypeUtils.getNameSegments("spicy.example.that.uses.Many$Inner$Classes"));
-        assertEquals(List.of("even", "spicier", "with", "Inner", "Class", "andMethods"), TypeUtils.getNameSegments("even.spicier.with.Inner$Class.andMethods"));
-    }
-
-    @Test
-    public void getPackageNameFromNameSegmentsTest() {
-        assertEquals("normal.package.setup.for.a", TypeUtils.getPackageNameFromNameSegments(List.of("normal", "package", "setup", "for", "a", "Class")));
-    }
-
-    @Test
-    public void getClassNameFromNameSegmentsTest() {
-        assertEquals("Class", TypeUtils.getClassNameFromNameSegments(List.of("normal", "package", "setup", "for", "a", "Class")));
-    }
-
-    @Test
-    public void fieldDescriptorNamesToSourceCodeNamesTest() {
-        assertEquals(List.of("byte[]", "int"), TypeUtils.fieldDescriptorNamesToSourceCodeNames(List.of("[B", "int")));
-        assertEquals(List.of("char[][]"), TypeUtils.fieldDescriptorNamesToSourceCodeNames(List.of("[[C")));
-        assertEquals(List.of("SuperCoolClass[][]"), TypeUtils.fieldDescriptorNamesToSourceCodeNames(List.of("[[com.google.SuperCoolClass")));
-        assertEquals(List.of("Type"), TypeUtils.fieldDescriptorNamesToSourceCodeNames(List.of("Type<with parameters>")));
-    }
-
-    @Test
-    public void isStandardTypeTest() {
-        assertTrue(TypeUtils.isStandardType("Comparable"));
-        assertFalse(TypeUtils.isStandardType("Other"));
-    }
-
-    @Test
-    public void isStandardTypeArrayTest() {
-        assertTrue(TypeUtils.isStandardTypeArray("Object[]"));
-        assertFalse(TypeUtils.isStandardTypeArray("Object"));
-        assertFalse(TypeUtils.isStandardTypeArray("AnyOtherObject"));
-        assertFalse(TypeUtils.isStandardTypeArray("AnyOtherObjectArray[]"));
-    }
-
-    @Test
-    public void hasJPTypeEllipsisTest() {
-        assertTrue(TypeUtils.hasEllipsis("Integer..."));
-        assertFalse(TypeUtils.hasEllipsis("Integer"));
-    }
 }
