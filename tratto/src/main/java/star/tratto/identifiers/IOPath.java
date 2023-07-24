77a4993971a5efe4448615878f1c5d9d5002a9c0
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-07-24 14:40:01 -0700

    re-purposed IOPath to store useful directories and file names

--- tratto/src/main/java/star/tratto/identifiers/IOPath.java
+++ tratto/src/main/java/star/tratto/identifiers/IOPath.java
@@ -1,24 +1,35 @@
 package star.tratto.identifiers;
 
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
+/**
+ * An enum to store useful directories and file names used in the dataset
+ * generation process.
+ */
 public enum IOPath {
-    OUTPUT(Paths.get("target", "output").toString()),
-    OUTPUT_DATASET(Paths.get("target", "output", "dataset").toString()),
-    RESOURCES(Paths.get("src", "main", "resources", "projects-source").toString()),
-    REPOS(Paths.get("src", "main", "java", "star", "tratto", "data", "repos").toString()),
-    ORACLES_DATASET(Paths.get("src", "main", "resources", "oracles-dataset").toString());
+    // directories
+    ORACLES_DATASET(Paths.get("src", "main", "resources", "oracles-dataset")),
+    OUTPUT(Paths.get("target", "output")),
+    OUTPUT_DATASET(Paths.get("target", "output", "dataset")),
+    REPOS(Paths.get("src", "main", "java", "star", "tratto", "data", "repos")),
+    RESOURCES(Paths.get("src", "main", "resources", "projects-source")),
+    // files
+    ARRAY_METHODS(IOPath.REPOS.getPath().resolve("array_methods.json")),
+    IGNORE_FILE(IOPath.REPOS.getPath().resolve("ignore_file.json")),
+    INPUT_PROJECTS(IOPath.REPOS.getPath().resolve("input_projects.json")),
+    NON_VARIABLE_TOKENS(IOPath.REPOS.getPath().resolve("non_variable_tokens")),
+    ORACLE_DATAPOINTS(IOPath.REPOS.getPath().resolve("oracle_datapoints")),
+    TOKENS_GENERAL_VALUES(IOPath.REPOS.getPath().resolve("tokens_general_values")),
+    TOKENS_GRAMMAR(IOPath.REPOS.getPath().resolve("tokens_grammar"));
 
-    private final String path;
+    private final Path path;
 
-    IOPath(String path) {
-        this.path = Paths.get(
-                System.getProperty("user.dir"),
-                path
-        ).toString();
+    IOPath(Path path) {
+        this.path = path;
     }
 
-    public String getValue() {
+    public Path getPath() {
         return this.path;
     }
 }
