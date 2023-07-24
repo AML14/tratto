266ec5b61521309441d2d08d745549c181ec26fa
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-07-24 16:38:12 -0700

    Linted ProjectParser

--- tratto/src/main/java/star/tratto/data/oracles/json/parsers/ProjectParser.java
+++ tratto/src/main/java/star/tratto/data/oracles/json/parsers/ProjectParser.java
@@ -11,25 +11,26 @@ import com.fasterxml.jackson.databind.module.SimpleModule;
 import star.tratto.data.oracles.Project;
 import star.tratto.data.oracles.json.deserializers.ProjectDeserializer;
 
-import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
- * A helper class that deserializes the project's information in JSON format, generating the corresponding Java
- * representations.
+ * A helper class that deserializes the project's information in JSON format
+ * and generates the corresponding Java representations.
  */
 public class ProjectParser {
-
     /**
-     * Given the path to the JSON file that lists the Java projects to analyze, the method deserializes the list of
-     * JSON project objects contained in the file and generates a list of corresponding Java project records
-     * @return a list of Java project records {@link Project}, representing the deserialization of the list of the JSON
-     * project objects contained in the JSON file pointed by the string path passed to the function
+     * Gets a list of project objects from a JSON list of project paths.
+     *
+     * @param jsonProjects file of projects to analyze
+     * @return list of project information
      */
-    public static List<Project> initialize(String jsonFilePath) {
-        // Instantiate an *ObjectMapper* to parse a JSON file
+    public static List<Project> initialize(Path jsonProjects) {
         ObjectMapper objectMapper = new ObjectMapper();
         // Generate a specific deserializer for the list of input projects JSON objects
         // that the system must parse
@@ -52,39 +53,20 @@ public class ProjectParser {
         // Parse the JSON file and generate a list of input projects
         try {
             List<Project> projectList = objectMapper.readValue(
-                    new File(jsonFilePath),
+                    jsonProjects.toFile(),
                     new TypeReference<>() {}
             );
-            // Filter the projects that effectively exists in the resources
-            return checkProjectsExist(projectList);
+            return projectList
+                    .stream()
+                    .filter(p -> {
+                        Path projectDir = Paths.get(p.getProjectPath());
+                        return Files.exists(projectDir) && Files.isDirectory(projectDir);
+                    })
+                    .collect(Collectors.toList());
         } catch (IOException e) {
             System.err.println("Unexpected error in processing the JSON file of the input projects.");
             e.printStackTrace();
+            return new ArrayList<>();
         }
-        return new ArrayList<>();
-    }
-
-    /**
-     * The method checks that each Java project record in the list passed to function points to a real Java project
-     * saved within the current Java program.
-     * @param projects the list of Java project records to check
-     * @return a list containing the only Java project records that point to real Java project saved within the current
-     * Java program
-     */
-    private static List<Project> checkProjectsExist(List<Project> projects) {
-        List<Project> existingProjects = new ArrayList<>();
-        for (Project project : projects) {
-            File projectDir = new File(project.getProjectPath());
-            if (projectDir.exists() && projectDir.isDirectory()) {
-                existingProjects.add(project);
-            } else {
-                String errorMessage = String.format(
-                        "The project %s is not present within the resources folder.",
-                        project.getProjectName()
-                );
-                System.err.println(errorMessage);
-            }
-        }
-        return existingProjects;
     }
 }
