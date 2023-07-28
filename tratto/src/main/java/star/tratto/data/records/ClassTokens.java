3558a34f05cd5879217b1a795d776fcaf7e51c7b
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-07-28 14:25:57 -0700

    setup basic ClassTokens record

--- tratto/src/main/java/star/tratto/data/records/ClassTokens.java
+++ tratto/src/main/java/star/tratto/data/records/ClassTokens.java
@@ -1,4 +1,11 @@
 package star.tratto.data.records;
 
-public record ClassTokens() {
+/**
+ * This record stores basic information of a Java class. Includes the class
+ * name (className) and package name (packageName).
+ *
+ * @param className the name of a Java class
+ * @param packageName the corresponding package of the Java class
+ */
+public record ClassTokens(String className, String packageName) {
 }
