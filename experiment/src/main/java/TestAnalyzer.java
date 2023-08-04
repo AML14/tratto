6a904d4932138a3723de89830ae5d06d91f5ec08
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-08-04 09:30:53 -0700

    added class JavaDoc to TestAnalyzer

--- experiment/src/main/java/TestAnalyzer.java
+++ experiment/src/main/java/TestAnalyzer.java
@@ -1,2 +1,11 @@
+/**
+ * This class provides static methods for calculating the mutation score of a
+ * given test suite, and classifying each test case based on whether a test
+ * passes or fails.
+ */
 public class TestAnalyzer {
+    // private constructor to avoid creating an instance of this class
+    private TestAnalyzer() {
+        throw new UnsupportedOperationException("This class cannot be instantiated.");
+    }
 }
