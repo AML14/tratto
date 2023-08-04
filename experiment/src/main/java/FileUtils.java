9564b2ee3e92b888e790b65ebbbf6e5b5ebb4bef
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-08-04 14:21:32 -0700

    added basic outline for FileUtils, might be deleted later.

--- experiment/src/main/java/FileUtils.java
+++ experiment/src/main/java/FileUtils.java
@@ -1,2 +1,9 @@
+/**
+ * This class provides a collection of static methods for a variety of file
+ * input and output utilities, such as: creating, copying, moving, writing,
+ * reading, etc.
+ */
 public class FileUtils {
+    // Should we copy-paste the utility from FileUtils in the main tratto
+    // project? Or is there a good way to import that file to this class.
 }
