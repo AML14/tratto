48d6416581991cc9cee23b77fb47e15b99e5a8fa
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-08-04 13:23:41 -0700

    added isExceptional field to distinguish between normal assertion oracles and exceptional oracles

--- experiment/src/main/java/OracleOutput.java
+++ experiment/src/main/java/OracleOutput.java
@@ -3,10 +3,13 @@
  *
  * @param className the binary name of the declaring class under test
  * @param methodSignature the method under test
+ * @param isExceptional a boolean which indicates if the oracle represents
+ *                      an exceptional behavior
  * @param oracle a generated oracle
  */
 public record OracleOutput(
         String className,
         String methodSignature,
+        boolean isExceptional,
         String oracle
 ) {}
