91b7fffe8cd3c966b4e734466aeafa71e3440ef9
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-08-07 14:49:51 -0700

    wrote test to validate removeOracles behavior

--- experiment/src/test/java/TestUtilsTest.java
+++ experiment/src/test/java/TestUtilsTest.java
@@ -1,7 +1,7 @@
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.body.MethodDeclaration;
-import com.github.javaparser.ast.stmt.AssertStmt;
+import com.github.javaparser.ast.expr.Expression;
+import com.github.javaparser.ast.expr.MethodCallExpr;
 import com.github.javaparser.ast.stmt.Statement;
 import org.junit.jupiter.api.Test;
 
@@ -15,10 +15,44 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TestUtilsTest {
     private static final Path examplePath = Paths.get("src/test/java/example");
     private static final Path outputPath = Paths.get("output");
+    private static final List<String> allJUnitTestMethods = List.of(
+            "assertArrayEquals",
+            "assertEquals",
+            "assertFalse",
+            "assertNotNull",
+            "assertNotSame",
+            "assertNull",
+            "assertSame",
+            "assertThat",
+            "assertTrue",
+            "fail"
+    );
 
     @Test
     public void removeOraclesTest() {
         TestUtils.removeOracles(examplePath);
-//        FileUtils.deleteDirectory(outputPath.resolve("evosuite-prefix"));
+        try {
+            CompilationUnit cu = StaticJavaParser.parse(outputPath.resolve("evosuite-prefix/ExampleTest.java"));
+            cu.findAll(Statement.class)
+                    .forEach(statement -> {
+                        // check all assert statements have been removed
+                        if (statement.isAssertStmt()) {
+                            fail();
+                        }
+                        // check all JUnit methods have been removed
+                        if (statement.isExpressionStmt()) {
+                            Expression expression = statement.asExpressionStmt().getExpression();
+                            if (expression.isMethodCallExpr()) {
+                                MethodCallExpr methodCallExpr = expression.asMethodCallExpr();
+                                if (allJUnitTestMethods.contains(methodCallExpr.getNameAsString())) {
+                                    fail();
+                                }
+                            }
+                        }
+                    });
+        } catch (IOException e) {
+            fail();
+        }
+        FileUtils.deleteDirectory(outputPath.resolve("evosuite-prefix"));
     }
 }
