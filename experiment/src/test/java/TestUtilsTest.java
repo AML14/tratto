fa1dc828c34fe3051f0519c1e438b44f370276db
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-08-07 13:43:44 -0700

    modified TestUtilsTest to use new CompilationUnit arguments

--- experiment/src/test/java/TestUtilsTest.java
+++ experiment/src/test/java/TestUtilsTest.java
@@ -1,6 +1,7 @@
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.stmt.ExpressionStmt;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.stmt.AssertStmt;
 import com.github.javaparser.ast.stmt.Statement;
 import org.junit.jupiter.api.Test;
 
@@ -18,33 +19,12 @@ public class TestUtilsTest {
     @Test
     public void removeAssertionOraclesTest() throws IOException {
         CompilationUnit cu = StaticJavaParser.parse(examplePath.resolve("ExampleTest.java"));
-        System.out.println(cu);
-//        // Creating the method body as a list of statements
-//        List<Statement> statements = List.of(
-//                new ExpressionStmt(
-//                        StaticJavaParser.parseExpression("List<String> nonEmptyList = List.of(\"example\");")),
-//                new ExpressionStmt(
-//                        StaticJavaParser.parseExpression("assertEquals(\"example\", nonEmptyList.get(0));"))
-//        );
-//
-//        // Creating the method declaration
-//        MethodDeclaration methodDeclaration = new MethodDeclaration(
-//                NodeList.nodeList(Modifier.publicModifier()),
-//                new NodeList<AnnotationExpr>().add(new NameExpr("Test")),
-//                new NodeList<>(),
-//                StaticJavaParser.parseType("void"),
-//                "assertionTest",
-//                new NodeList<>(),
-//                new BlockStmt(statements)
-//        );
-
-        // Print the generated method declaration
-//        System.out.println(methodDeclaration.toString());
+        TestUtils.removeAssertionOracles(cu);
     }
 
     @Test
     public void removeOraclesTest() {
         TestUtils.removeOracles(examplePath);
-//        FileUtils.deleteDirectory(outputPath.resolve("evosuite-prefix"));
+        FileUtils.deleteDirectory(outputPath.resolve("evosuite-prefix"));
     }
 }
