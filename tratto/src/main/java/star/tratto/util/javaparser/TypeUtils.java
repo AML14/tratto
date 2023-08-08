4da6968c38ddb74fcece4615e6e69a846a662683
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-08-08 12:04:29 -0700

    added clarifying comment for primitive conversion

--- tratto/src/main/java/star/tratto/util/javaparser/TypeUtils.java
+++ tratto/src/main/java/star/tratto/util/javaparser/TypeUtils.java
@@ -159,6 +159,7 @@ public class TypeUtils {
         fieldDescriptor = removeTypeArgumentsAndSemicolon(fieldDescriptor);
         if (hasPrimitive(fieldDescriptor)) {
             // use plume-lib to convert between primitive field descriptors and binary names
+            // (equivalent for primitive types)
             fieldDescriptor = Signatures.fieldDescriptorToBinaryName(fieldDescriptor);
         } else {
             // manually convert array and then get class name
