20ea636df8fb715962d771e4d1913630e7a53729
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-08-09 09:16:50 -0700

    setup path output in evosuite.sh

--- experiment/generator/evosuite.sh
+++ experiment/generator/evosuite.sh
@@ -1,2 +1,7 @@
 # This script generates a test suite using EvoSuite and saves the output to
 # experiment/output/evosuite-test.
+
+# setup output path
+EXPERIMENT_PATH=$(dirname "$(pwd)")
+TEST_OUTPUT_PATH="$EXPERIMENT_PATH/output/evosuite-test"
+mkdir -p "$TEST_OUTPUT_PATH"
