694381d49d4adad62b8dbef6f1a3ed39fbe538da
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-08-10 16:48:53 -0700

    configured java8 setup

--- experiment/generator/evosuite.sh
+++ experiment/generator/evosuite.sh
@@ -5,3 +5,13 @@
 EXPERIMENT_PATH=$(dirname "$(pwd)")
 TEST_OUTPUT_PATH="$EXPERIMENT_PATH/output/evosuite-test"
 mkdir -p "$TEST_OUTPUT_PATH"
+# check if jdk8 is configured
+JAVA8_HOME="$(pwd)/resources/jdk1.8.0_3281.jdk/Contents/Home"
+if [ ! -d "$JAVA8_HOME" ]; then
+  echo -e "Error: JDK8 directory does not exist. See \"Setup\" in."
+  exit 1
+fi
+
+# setup evosuite commands
+EVOSUITE="java -jar $(pwd)/resources/evosuite-1.0.6.jar"
+(export JAVA_HOME=$JAVA8_HOME ; $EVOSUITE)
