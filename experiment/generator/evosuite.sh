8f245a4ba31b6bd4fb5933e9852fe8acffa3c508
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-08-11 09:38:52 -0700

    grouped argument checker

--- experiment/generator/evosuite.sh
+++ experiment/generator/evosuite.sh
@@ -6,7 +6,7 @@
 JAVA8_HOME="$(pwd)/resources/jdk1.8.0_381.jdk/Contents/Home"
 
 
-# check arguments
+# argument checker
 if [ ! $# -eq 1 ]; then
   echo -e "Incorrect number of arguments to use EvoSuite (must specify input path)."
   exit 1
@@ -15,18 +15,19 @@ if [ ! -d "$1" ]; then
   echo -e "Input path \"$1\" does not exist."
   exit 1
 fi
-
-# setup output path
-INPUT_PATH=$1
-EXPERIMENT_PATH=$(dirname "$(pwd)")
-OUTPUT_PATH="$EXPERIMENT_PATH/output/evosuite-test"
-mkdir -p "$OUTPUT_PATH"
 # check if jdk8 is configured
 if [ ! -d "$JAVA8_HOME" ]; then
   echo -e "Error: JDK8 directory \"$JAVA8_HOME\" does not exist.
 See \"Setup\" in the experiment README.md for further details."
   exit 1
 fi
+
+
+# setup output path
+INPUT_PATH=$1
+EXPERIMENT_PATH=$(dirname "$(pwd)")
+OUTPUT_PATH="$EXPERIMENT_PATH/output/evosuite-test"
+mkdir -p "$OUTPUT_PATH"
 
 # setup evosuite commands
 EVOSUITE="java -jar $(pwd)/resources/evosuite-1.0.6.jar"
