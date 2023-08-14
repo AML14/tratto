4e34ca5c2c13639741f5bbf3c43b9041cdac3c01
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-08-14 09:51:29 -0700

    added argument checker to jdoctor script

--- experiment/generator/jdoctor.sh
+++ experiment/generator/jdoctor.sh
@@ -1,2 +1,14 @@
 # This script generates a list of axiomatic oracles using JDoctor.
 # Should output a list of OracleOutput.
+
+# argument check
+if [ ! $# -eq 3 ]; then
+  echo -e "Incorrect number of arguments. Expected 3 arguments, but got $#".
+  exit 1
+elif [ ! -d "$2" ]; then
+  echo -e "The source directory \"$2\" does not exist."
+  exit 1
+elif [ ! -d "$3" ]; then
+  echo -e "The system binaries path \"$3\" does not exist."
+  exit 1
+fi
