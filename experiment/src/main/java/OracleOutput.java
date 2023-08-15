d891e3c8645c3415b0e98707d30ab8f30fccbbee
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-08-15 14:51:06 -0700

    modified OracleOutput record

--- experiment/src/main/java/OracleOutput.java
+++ experiment/src/main/java/OracleOutput.java
@@ -1,15 +1,17 @@
 /**
  * This record represents a generated oracle and contextual information.
  *
- * @param className the binary name of the declaring class under test
+ * @param className the fully qualified name of the declaring class under test
  * @param methodSignature the method under test
- * @param isExceptional a boolean which indicates if the oracle represents
- *                      an exceptional behavior
+ * @param oracleType the type of oracle
  * @param oracle a generated oracle
+ * @param exception the exception class thrown by an exceptional oracle. An
+ *                  empty string if the oracle is not exceptional.
  */
 public record OracleOutput(
         String className,
         String methodSignature,
-        boolean isExceptional,
-        String oracle
+        String oracleType,
+        String oracle,
+        String exception
 ) {}
