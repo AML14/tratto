c3df05c83451f2f4f46b6bcc67aa9f04388aaf0f
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-08-15 09:56:49 -0700

    added prefix to OracleOutput

--- experiment/src/main/java/OracleOutput.java
+++ experiment/src/main/java/OracleOutput.java
@@ -5,11 +5,14 @@
  * @param methodSignature the method under test
  * @param isExceptional a boolean which indicates if the oracle represents
  *                      an exceptional behavior
+ * @param prefix a test prefix corresponding to the generated oracle. An empty
+ *               string if the oracle is axiomatic.
  * @param oracle a generated oracle
  */
 public record OracleOutput(
         String className,
         String methodSignature,
         boolean isExceptional,
+        String prefix,
         String oracle
 ) {}
