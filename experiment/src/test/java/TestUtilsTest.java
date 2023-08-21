cf204146b0a7874d17741ae958de6a5288582f41
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-08-21 09:52:44 -0700

    set methodsignature example to use FQN in methodSignature

--- experiment/src/test/java/TestUtilsTest.java
+++ experiment/src/test/java/TestUtilsTest.java
@@ -69,7 +69,7 @@ public class TestUtilsTest {
                 ),
                 new OracleOutput(
                         "java.lang.Integer",
-                        "parseInt(String)",
+                        "parseInt(java.lang.String)",
                         "EXCEPT_POST",
                         "",
                         "assertTrue((s == null) == false)",
@@ -102,7 +102,7 @@ public class TestUtilsTest {
                 ),
                 new OracleOutput(
                         "java.lang.Integer",
-                        "parseInt(String)",
+                        "parseInt(java.lang.String)",
                         "",
                         """
                                 @Test
