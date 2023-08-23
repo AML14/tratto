77e6e11e1f389e05b9fcd1603ef4fe843673f7d6
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-08-23 16:06:23 -0700

    made example test more complicated

--- experiment/src/test/resources/prefix/ExamplePrefix.java
+++ experiment/src/test/resources/prefix/ExamplePrefix.java
@@ -3,25 +3,36 @@ package example;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+
 public class ExamplePrefix {
     @Test
     @Disabled
-    public void assertionTest() {
+    public void assertionTest() throws Throwable {
         int primitiveInt = 5;
         Integer objectInt = Integer.valueOf(primitiveInt);
     }
 
     @Test
     @Disabled
-    public void assertionNonStaticTest() {
+    public void assertionNonStaticTest() throws Throwable {
         Integer objectInt = 5;
         objectInt.intValue();
     }
 
     @Test
     @Disabled
-    public void exceptionalTest() {
+    public void exceptionalTest() throws Throwable {
         String integerToParse = null;
         int correspondingInteger = Integer.parseInt(integerToParse);
+    }
+
+    @Test
+    @Disabled
+    public void everythingTest() throws Throwable {
+        Class<?> clazz = Integer.class;
+        String methodName = "compare";
+        Class<?>[] parameters = {int.class, int.class};
+        Method method = clazz.getMethod(methodName, parameters);
     }
 }
