5660bcb31b403159e9a04529026d987224286b88
davide <davide.molinelli@mail.polimi.it>
2023-08-29 23:05:03 +0200

    fixed bug on toga.sh (deleted content)

--- experiment/generator/toga.sh
+++ experiment/generator/toga.sh
@@ -21,6 +21,20 @@ TOGA_PROJECT="${ROOT_DIR}${SEPARATOR}generator${SEPARATOR}resources${SEPARATOR}t
 TOGA_INPUT="${ROOT_DIR}${SEPARATOR}output${SEPARATOR}toga${SEPARATOR}input"
 TOGA_OUTPUT="${ROOT_DIR}${SEPARATOR}output${SEPARATOR}toga${SEPARATOR}output"
 
+java -jar "generator${SEPARATOR}resources${SEPARATOR}experiment.jar" toga generate_tog_inputs $SRC_PATH $FULLY_QUALIFIED_NAME
 
+cd "${TOGA_PROJECT}"
+
+python3 toga.py "${TOGA_INPUT}${SEPARATOR}toga_input.csv" "${TOGA_INPUT}${SEPARATOR}toga_metadata.csv"
+
+if [ ! -d "$TOGA_OUTPUT" ]; then
+    # If it doesn't exist, create the folder
+    mkdir -p "$TOGA_OUTPUT"
+    echo "Folder created: $TOGA_OUTPUT"
+fi
+
+mv "${TOGA_PROJECT}${SEPARATOR}oracle_preds.csv" "${TOGA_OUTPUT}"
+
+cd "${ROOT_DIR}"
 
 java -jar "generator${SEPARATOR}resources${SEPARATOR}experiment.jar" toga generate_oracle_outputs $SRC_PATH
