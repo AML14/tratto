3a6f3d7c6041cf9df87024ac992a2ee003e066d5
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-08-30 12:19:10 -0700

    fixed indentation format

--- experiment/src/main/java/data/JDoctorOutput.java
+++ experiment/src/main/java/data/JDoctorOutput.java
@@ -5,45 +5,45 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
 public record JDoctorOutput(
-    @JsonProperty("signature") String signature,
-    @JsonProperty("name") String name,
-    @JsonProperty("containingClass") Type containingClass,
-    @JsonProperty("targetClass") String targetClass,
-    @JsonProperty("isVarArgs") boolean isVarArgs,
-    @JsonProperty("returnType") Type returnType,
-    @JsonProperty("parameters") List<Parameter> parameters,
-    @JsonProperty("paramTags") List<ParamTag> paramTags,
-    @JsonProperty("returnTag") ReturnTag returnTag,
-    @JsonProperty("throwsTags") List<ThrowsTag> throwsTags
+        @JsonProperty("signature") String signature,
+        @JsonProperty("name") String name,
+        @JsonProperty("containingClass") Type containingClass,
+        @JsonProperty("targetClass") String targetClass,
+        @JsonProperty("isVarArgs") boolean isVarArgs,
+        @JsonProperty("returnType") Type returnType,
+        @JsonProperty("parameters") List<Parameter> parameters,
+        @JsonProperty("paramTags") List<ParamTag> paramTags,
+        @JsonProperty("returnTag") ReturnTag returnTag,
+        @JsonProperty("throwsTags") List<ThrowsTag> throwsTags
 ) {
     public record ParamTag(
-        @JsonProperty("comment") String comment,
-        @JsonProperty("kind") String kind,
-        @JsonProperty("condition") String condition
+            @JsonProperty("comment") String comment,
+            @JsonProperty("kind") String kind,
+            @JsonProperty("condition") String condition
 
     ) {}
 
     public record ThrowsTag(
-        @JsonProperty("comment") String comment,
-        @JsonProperty("kind") String kind,
-        @JsonProperty("condition") String condition
+            @JsonProperty("comment") String comment,
+            @JsonProperty("kind") String kind,
+            @JsonProperty("condition") String condition
     ) {}
 
     public record ReturnTag(
-        @JsonProperty("comment") String comment,
-        @JsonProperty("kind") String kind,
-        @JsonProperty("condition") String condition
+            @JsonProperty("comment") String comment,
+            @JsonProperty("kind") String kind,
+            @JsonProperty("condition") String condition
     ) {}
 
     public record Parameter(
-        @JsonProperty("type") Type type,
-        @JsonProperty("name") String name,
-        @JsonProperty("nullable") boolean isNullable
+            @JsonProperty("type") Type type,
+            @JsonProperty("name") String name,
+            @JsonProperty("nullable") boolean isNullable
     ) {}
 
     public record Type(
-        @JsonProperty("qualifiedName") String fullyQualifiedName,
-        @JsonProperty("name") String name,
-        @JsonProperty("isArray") boolean isArray
+            @JsonProperty("qualifiedName") String fullyQualifiedName,
+            @JsonProperty("name") String name,
+            @JsonProperty("isArray") boolean isArray
     ) {}
 }
