8947bba30af287dc9ef776947894866d7988e5ba
davide <davide.molinelli@mail.polimi.it>
2023-08-30 13:55:45 +0200

    fixed bug on toga script

--- experiment/generator/toga.sh
+++ experiment/generator/toga.sh
@@ -17,9 +17,12 @@ fi
 ROOT_DIR=$(pwd)
 FULLY_QUALIFIED_NAME="$1"
 SRC_PATH="$2"
+EVOSUITE_OUTPUT="$3"
 TOGA_PROJECT="${ROOT_DIR}${SEPARATOR}generator${SEPARATOR}resources${SEPARATOR}toga"
 TOGA_INPUT="${ROOT_DIR}${SEPARATOR}output${SEPARATOR}toga${SEPARATOR}input"
 TOGA_OUTPUT="${ROOT_DIR}${SEPARATOR}output${SEPARATOR}toga${SEPARATOR}output"
+
+java -jar "generator${SEPARATOR}resources${SEPARATOR}experiment.jar" toga remove_oracles $EVOSUITE_OUTPUT $FULLY_QUALIFIED_NAME
 
 java -jar "generator${SEPARATOR}resources${SEPARATOR}experiment.jar" toga generate_tog_inputs $SRC_PATH $FULLY_QUALIFIED_NAME
 
