a8c902d7822f2159c76b631ee4c31ed36ed8c167
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-08-29 18:39:21 -0700

    fixed implementation to use Java 8

--- experiment/generator/jdoctor.sh
+++ experiment/generator/jdoctor.sh
@@ -2,6 +2,10 @@
 # Should output a list of OracleOutput.
 RESOURCES_DIR="$(dirname "$0")/resources"
 
+# ----- SETUP -----
+# Set this path to the "Home" directory in a local JDK8.
+JAVA8_HOME="$RESOURCES_DIR/jdk1.8.0_381.jdk/Contents/Home"
+
 # argument check
 if [ ! $# -eq 3 ]; then
   echo -e "(JDOCTOR) Incorrect number of arguments. Expected 3 arguments, but got $#".
@@ -12,6 +16,9 @@ elif [ ! -d "$2" ]; then
 elif [ ! -d "$3" ]; then
   echo -e "(JDOCTOR) The system binaries path \"$3\" does not exist."
   exit 1
+elif [ ! -d "$JAVA8_HOME" ]; then
+  echo -e "(JDOCTOR) Error: JDK8 Home directory \"$JAVA8_HOME\" does not exist."
+  exit 1
 fi
 
 TARGET_CLASS="$1"  # fully-qualified name of target class
@@ -21,4 +28,4 @@ OUTPUT_DIR="$RESOURCES_DIR/../../output"
 JDOCTOR="java -jar $RESOURCES_DIR/toradocu-1.0-all.jar"
 mkdir -p "$OUTPUT_DIR"
 # use JDoctor to generate oracles
-$JDOCTOR --target-class "$TARGET_CLASS" --source-dir "$SRC_DIR" --class-dir "$CLASS_DIR"
+(export JAVA_HOME=$JAVA8_HOME ; $JDOCTOR --target-class "$TARGET_CLASS" --source-dir "$SRC_DIR" --class-dir "$CLASS_DIR")
