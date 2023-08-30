5bca1b962210f31fe33d4e21a2cd0b5066fbf40f
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-08-29 17:09:58 -0700

    clarified error messages in jdoctor.sh

--- experiment/generator/jdoctor.sh
+++ experiment/generator/jdoctor.sh
@@ -4,13 +4,13 @@ RESOURCES_DIR="$(dirname "$0")/resources"
 
 # argument check
 if [ ! $# -eq 3 ]; then
-  echo -e "Incorrect number of arguments. Expected 3 arguments, but got $#".
+  echo -e "(JDOCTOR) Incorrect number of arguments. Expected 3 arguments, but got $#".
   exit 1
 elif [ ! -d "$2" ]; then
-  echo -e "The source directory \"$2\" does not exist."
+  echo -e "(JDOCTOR) The source directory \"$2\" does not exist."
   exit 1
 elif [ ! -d "$3" ]; then
-  echo -e "The system binaries path \"$3\" does not exist."
+  echo -e "(JDOCTOR) The system binaries path \"$3\" does not exist."
   exit 1
 fi
 
