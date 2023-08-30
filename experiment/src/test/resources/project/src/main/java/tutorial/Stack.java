ba227d2f85fb65dae3d27219763acda574458124
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-08-30 09:43:03 -0700

    added javadoc to Stack example class for push and pop methods

--- experiment/src/test/resources/project/src/main/java/tutorial/Stack.java
+++ experiment/src/test/resources/project/src/main/java/tutorial/Stack.java
@@ -7,12 +7,24 @@ public class Stack<T> {
     private int pointer  = 0;
     private T[] objects = (T[]) new Object[capacity];
 
+    /**
+     * Adds a new object to the stack.
+     *
+     * @param o an object
+     * @throws RuntimeException if o is null
+     */
     public void push(T o) {
         if (pointer >= capacity)
             throw new RuntimeException("Stack exceeded capacity!");
         objects[pointer++] = o;
     }
 
+    /**
+     * Gets and removes the most recent object pushed onto the stack.
+     *
+     * @return the most recent object pushed to the stack
+     * @throws EmptyStackException if the stack is empty
+     */
     public T pop() {
         if (pointer <= 0)
             throw new EmptyStackException();
