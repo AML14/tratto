59186733452627bb46902b16a90cac30c858d50f
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-08-31 15:58:40 -0700

    fixed oracle_output paths

--- experiment/experiment.sh
+++ experiment/experiment.sh
@@ -13,14 +13,14 @@ else
     SEPARATOR="\\"
 fi
 
-ROOT_DIR=$(pwd)
+ROOT_DIR=$(dirname "$(realpath "$0")")
 TOG=$1
 TARGET_CLASS=$2
 SRC_DIR=$3
 BIN_DIR=$4
 QUALIFIERS="${TARGET_CLASS%.*}"
 EVOSUITE_OUTPUT="${ROOT_DIR}${SEPARATOR}output${SEPARATOR}evosuite-tests${SEPARATOR}${QUALIFIERS//./$SEPARATOR}"
-EXPERIMENT="java -jar ./generator/resources/experiment.jar"
+EXPERIMENT="java -jar $ROOT_DIR/generator/resources/experiment.jar"
 
 # check if given directories exist
 if [ ! -d "$SRC_DIR" ]; then
@@ -51,10 +51,10 @@ $EXPERIMENT "$TOG" "remove_oracles" "$EVOSUITE_OUTPUT" "$TARGET_CLASS"
 # generate oracles using TOG
 if [ "${TOG}" == "jdoctor" ]; then
   bash ./generator/jdoctor.sh "${TARGET_CLASS}" "${SRC_DIR}" "${BIN_DIR}"
-  ORACLE_OUTPUT="./output/jdoctor/oracle/oracle_outputs.json"
+  ORACLE_OUTPUT="$ROOT_DIR/output/jdoctor/oracle_outputs.json"
 elif [ "${TOG}" == "toga" ]; then
   bash ./generator/toga.sh "${TARGET_CLASS}" "${SRC_DIR}" "${EVOSUITE_OUTPUT}"
-  ORACLE_OUTPUT="./output/toga/oracle/oracle_outputs.json"
+  ORACLE_OUTPUT="$ROOT_DIR/output/toga/oracle/oracle_outputs.json"
 elif [ "${TOG}" == "tratto" ]; then
   bash ./generator/tratto.sh "${TARGET_CLASS}" "${SRC_DIR}"
   ORACLE_OUTPUT=""
