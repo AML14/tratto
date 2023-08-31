e08d9076b21b6961233307e5ef870990c3d5e2b0
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-08-31 13:38:09 -0700

    relativized all paths

--- experiment/generator/evosuite.sh
+++ experiment/generator/evosuite.sh
@@ -7,7 +7,8 @@
 JDK8_NAME="jdk-1.8.jdk"
 
 # find JDK8 directory
-RESOURCES_DIR="$(dirname "$0")/resources"
+ROOT_DIR=$(dirname "$(dirname "$(realpath "$0")")")
+RESOURCES_DIR="$ROOT_DIR/generator/resources"
 JDK_DEFAULT_PATH=$(find "$RESOURCES_DIR" -type d -name 'jdk*.jdk' -print -quit)
 if [ -n "$JDK_DEFAULT_PATH" ]; then
   JDK_PATH=$(dirname "$JDK_DEFAULT_PATH")"/$JDK8_NAME"
@@ -18,7 +19,7 @@ if [ -n "$JDK_DEFAULT_PATH" ]; then
 else
   echo "(EVOSUITE) Unable to find a jdk directory. Please provide the complete path to the Java 8 JDK directory:"
   read -r JAVA8_FOLDER
-  JAVA8_HOME="${JAVA8_FOLDER}/Contents/Home"
+  JAVA8_HOME="$JAVA8_FOLDER/Contents/Home"
 fi
 
 # argument and setup check
@@ -36,12 +37,10 @@ fi
 # setup variables
 TARGET_CLASS="$1"  # fully-qualified name of target class
 TARGET_DIR="$2"  # directory of binary files of the system under test
-OUTPUT_DIR="$RESOURCES_DIR/../../output"
-EVOSUITE="${JAVA8_HOME}/bin/java -jar $RESOURCES_DIR/evosuite-1.0.6.jar"
+OUTPUT_DIR="$ROOT_DIR/output"
+EVOSUITE="$JAVA8_HOME/bin/java -jar $RESOURCES_DIR/evosuite-1.0.6.jar"
 
 # generate tests using EvoSuite
 mkdir -p "$OUTPUT_DIR"
-(export JAVA_HOME=$JAVA8_HOME; $EVOSUITE -class "$TARGET_CLASS" -projectCP "$TARGET_DIR")
-# move output to set directory
-mv "./evosuite-tests" "$OUTPUT_DIR"
-rm -r "./evosuite-report"
+(export JAVA_HOME=$JAVA8_HOME; $EVOSUITE -class "$TARGET_CLASS" -projectCP "$TARGET_DIR" -base_dir="$OUTPUT_DIR")
+rm -r "$ROOT_DIR/generator/evosuite-report"
