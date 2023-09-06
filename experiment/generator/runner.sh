f71b8d2cf51cac3ac0e9a932ac8df787ed8f0ea0
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-09-06 13:59:43 -0700

    updated paths

--- experiment/generator/runner.sh
+++ experiment/generator/runner.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-JDK8_NAME="jdk-8.jdk"
+# ----- SETUP -----
+# After adding the local JDK8 to the generator/resources directory,
+# set this field to the directory name.
+JDK8_NAME="jdk-1.8.jdk"
+
 ROOT_DIR=$(dirname "$(dirname "$(realpath "$0")")")
-RESOURCES_DIR="$ROOT_DIR/src/main/resources"
+RESOURCES_DIR="$ROOT_DIR/generator/resources"
 JAVA8_HOME="$RESOURCES_DIR/$JDK8_NAME/Contents/Home"
 JAVA8_BIN="$JAVA8_HOME/bin/java"
 JAVA8_C="$JAVA8_HOME/bin/javac"
+PROJECT_DIR="$1/../.."
+TOG=$2
+OUTPUT_DIR="$ROOT_DIR/output"
+TEST_DIR="$OUTPUT_DIR/tog-tests/$TOG"
+
+mv "$TEST_DIR" "$PROJECT_DIR"
+cd .. || exit 1
+cd "$PROJECT_DIR" || exit 1
 
 (export JAVA_HOME=$JAVA8_HOME;
 mvn dependency:copy-dependencies
