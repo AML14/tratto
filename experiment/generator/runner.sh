1abeca207e8b197104c9bcf3b6fccd5b0b3585c2
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-09-06 20:24:09 -0700

    fixed pathing in runner.sh

--- experiment/generator/runner.sh
+++ experiment/generator/runner.sh
@@ -7,22 +7,43 @@ JDK8_NAME="jdk-1.8.jdk"
 
 ROOT_DIR=$(dirname "$(dirname "$(realpath "$0")")")
 RESOURCES_DIR="$ROOT_DIR/generator/resources"
+OUTPUT_DIR="$ROOT_DIR/output"
 JAVA8_HOME="$RESOURCES_DIR/$JDK8_NAME/Contents/Home"
 JAVA8_BIN="$JAVA8_HOME/bin/java"
 JAVA8_C="$JAVA8_HOME/bin/javac"
-PROJECT_DIR="$1/../.."
-TOG=$2
-OUTPUT_DIR="$ROOT_DIR/output"
+TOG=$1
+SRC_DIR=$(realpath "$2")
+BIN_DIR=$(realpath "$3")
 TEST_DIR="$OUTPUT_DIR/tog-tests/$TOG"
 
-mv "$TEST_DIR" "$PROJECT_DIR"
-cd .. || exit 1
+IFS='/' read -ra SRC_ARR <<< "$SRC_DIR"
+IFS='/' read -ra BIN_ARR <<< "$BIN_DIR"
+PROJECT_DIR=""
+for i in "${!SRC_ARR[@]}" "${!BIN_ARR[@]}"; do
+  if [ "${SRC_ARR[i]}" != "${BIN_ARR[i]}" ]; then
+    break
+  fi
+  PROJECT_DIR="${PROJECT_DIR}/${SRC_ARR[i]}"
+done
+PROJECT_DIR="${PROJECT_DIR#/}"
+
+echo "$TOG"
+echo "$TEST_DIR"
+echo "$PROJECT_DIR"
+
+#find "$TEST_DIR" -type f -exec mv {} "$PROJECT_DIR" \;
+cp "$TEST_DIR" "$PROJECT_DIR"
 cd "$PROJECT_DIR" || exit 1
 
-(export JAVA_HOME=$JAVA8_HOME;
-mvn dependency:copy-dependencies
-export CLASSPATH="target/classes:evosuite-standalone-runtime-1.0.6.jar:evosuite-tests:target/dependency/junit-4.12.jar:target/dependency/hamcrest-core-1.3.jar"
-$JAVA8_C "$ROOT_DIR/evosuite-tests/tutorial/"*".java"
-echo "$ROOT_DIR/evosuite-tests/tutorial/"*".java"
-$JAVA8_BIN org.junit.runner.JUnitCore tutorial.Stack_ESTest
-)
+#
+#mv "$TEST_DIR" "$PROJECT_DIR"
+#cd .. || exit 1
+#cd "$PROJECT_DIR" || exit 1
+#
+#(export JAVA_HOME=$JAVA8_HOME;
+#mvn dependency:copy-dependencies
+#export CLASSPATH="target/classes:evosuite-standalone-runtime-1.0.6.jar:evosuite-tests:target/dependency/junit-4.12.jar:target/dependency/hamcrest-core-1.3.jar"
+#$JAVA8_C "$ROOT_DIR/evosuite-tests/tutorial/"*".java"
+#echo "$ROOT_DIR/evosuite-tests/tutorial/"*".java"
+#$JAVA8_BIN org.junit.runner.JUnitCore tutorial.Stack_ESTest
+#)
