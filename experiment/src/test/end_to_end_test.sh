62d16ec6ee5b3aed3b32920d21b339a647e5d306
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-09-13 09:53:00 -0700

    removed end_to_end_test.sh

--- experiment/src/test/end_to_end_test.sh
+++
@@ -1,3 +0,0 @@
-# This simple test is used to validate expected end-to-end behavior during development.
-
-bash ../../experiment.sh "jdoctor" tutorial.Stack resources/project/src resources/project/target/classes
