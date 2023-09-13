3c100f3772c72a2ce17f6f3dcc248937c547ea72
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-09-13 11:43:43 -0700

    added javadoc for fields in OperationType

--- experiment/src/main/java/data/OperationType.java
+++ experiment/src/main/java/data/OperationType.java
@@ -4,9 +4,14 @@ package data;
  * This enum defines all possible operations of the main Tog class.
  */
 public enum OperationType {
+    /** Removes oracles from a test suite. */
     REMOVE_ORACLES,
+    /** Adds oracles to a test prefix. */
     INSERT_ORACLES,
+    /** Preprocesses an input file to be passed to a TOG. */
     GENERATE_TOG_INPUTS,
+    /** Post-processes the output of a TOG into an {@link OracleOutput}. */
     GENERATE_ORACLE_OUTPUTS,
+    /** Converts an {@link OracleOutput} into a {@link TestOutput}. */
     GENERATE_TEST_OUTPUT
 }
