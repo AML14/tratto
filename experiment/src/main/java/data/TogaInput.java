7c3d145bc1133183cbc9d9b5345d10f83196221d
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-09-13 12:46:45 -0700

    added documentation for TogaInput

--- experiment/src/main/java/data/TogaInput.java
+++ experiment/src/main/java/data/TogaInput.java
@@ -1,5 +1,14 @@
 package data;
 
+/**
+ * A record for pre-processed context of the system under test. This context
+ * is then passed to TOGA as input.
+ *
+ * @param className the declaring class of {@code methodSignature}
+ * @param methodSignature the method under test
+ * @param prefix a test prefix
+ * @param testName the name of the test
+ */
 public record TogaInput(
         String className,
         String methodSignature,
