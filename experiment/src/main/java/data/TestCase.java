546c692e4d6a79a12e5bdde28a728c6145af3805
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-09-13 12:34:38 -0700

    added documentation for TestCase

--- experiment/src/main/java/data/TestCase.java
+++ experiment/src/main/java/data/TestCase.java
@@ -1,9 +1,21 @@
 package data;
 
+/**
+ * This record represents to a Java test case, with fields for the test name,
+ * test body, and whether the test passes or fails.
+ *
+ * @param testName the name of the method test case
+ * @param testBody the test body
+ * @param testResult the test result (either
+ */
 public record TestCase(
-        String testCase,
+        String testName,
+        String testBody,
         TestResult testResult
 ) {
+    /**
+     * An enum representing whether a test passes or fails.
+     */
     public enum TestResult {
         PASS,
         FAIL
