90399e09191a2916aa8d13557247a3d3a5262587
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-09-13 11:53:08 -0700

    removed prefix field from OracleOutput

--- experiment/src/main/java/data/OracleOutput.java
+++ experiment/src/main/java/data/OracleOutput.java
@@ -1,24 +1,25 @@
 package data;
 
 /**
- * This record represents a generated oracle and contextual information.
+ * This record represents a generated oracles and its corresponding contextual
+ * information.
  *
- * @param className the fully qualified name of the declaring class under test
+ * @param className the fully qualified name of the declaring class of
+ *                  {@code methodSignature}
  * @param methodSignature the method under test
  * @param oracleType the type of oracle
- * @param prefix the test prefix corresponding to a non-axiomatic oracle. Empty
- *               if the oracle is axiomatic.
  * @param oracle a generated oracle
- * @param exception the exception class thrown by an exceptional oracle. An
- *                  empty string if the oracle is not exceptional.
- * @param testName the name of the test (prefix) from which the assertion has
- *                 been produced. An empty string if the oracle is axiomatic.
+ * @param exception the type of exception thrown by an exceptional oracle.
+ *                  This field is an empty String if the oracle is not
+ *                  exceptional.
+ * @param testName the name of the test corresponding to a non-axiomatic
+ *                 oracle. This field is an empty String if the oracle is
+ *                 axiomatic.
  */
 public record OracleOutput(
         String className,
         String methodSignature,
         OracleType oracleType,
-        String prefix,
         String oracle,
         String exception,
         String testName
