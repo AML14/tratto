118b7b63eeedb0c1a4d9d2d61fae2c8a458e5851
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-09-14 16:21:55 -0700

    added setup jdk name to all necessary scripts

--- experiment/generator/evosuite.sh
+++ experiment/generator/evosuite.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 # This script generates a test suite using EvoSuite and saves the output to
 # "output/evosuite-tests".
+
+# ----- SETUP -----
+# After adding the local JDK8 to the generator/resources directory...
+# Set this field to the directory name.
+JDK8_NAME="jdk-1.8.jdk"
--- experiment/generator/jdoctor.sh
+++ experiment/generator/jdoctor.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 # This script generates a list of axiomatic oracles using JDoctor. Saves the
 # output to "output/jdoctor/oracle" as a list of OracleOutput records.
+
+# ----- SETUP -----
+# After adding the local JDK8 to the generator/resources directory...
+# Set this field to the directory name.
+JDK8_NAME="jdk-1.8.jdk"
