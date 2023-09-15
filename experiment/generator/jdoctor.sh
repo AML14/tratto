35c2c8c351dfdab6b67e10dc51e66e7034083995
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-09-15 09:26:28 -0700

    fixed experiment setup

--- experiment/generator/jdoctor.sh
+++ experiment/generator/jdoctor.sh
@@ -1,2 +1,8 @@
-# This script generates a list of axiomatic oracles using JDoctor.
-# Should output a list of OracleOutput.
+#!/bin/bash
+# This script generates a list of axiomatic oracles using JDoctor. Saves the
+# output to "output/jdoctor/oracle" as a list of OracleOutput records.
+
+# ----- SETUP -----
+# After adding the local JDK8 to the generator/resources directory...
+# Set this field to the directory name.
+JDK8_NAME="jdk-1.8.jdk"
--- experiment/src/test/resources/project/src/main/java/tutorial/Stack.java
+++ experiment/src/test/resources/project/src/main/java/tutorial/Stack.java
@@ -6,18 +6,39 @@ public class Stack<T> {
     private int capacity = 10;
     private int pointer  = 0;
     private T[] objects = (T[]) new Object[capacity];
-    
+
+    /**
+     * Adds a new object to the stack.
+     *
+     * @param o an object. Must not be null.
+     * @throws IllegalArgumentException if o is null
+     */
     public void push(T o) {
-	if(pointer >= capacity)
-	    throw new RuntimeException("Stack exceeded capacity!");
-	objects[pointer++] = o;
+        if (pointer >= capacity)
+            throw new RuntimeException("Stack exceeded capacity!");
+        if (o == null)
+            throw new IllegalArgumentException("The given object is null!");
+        objects[pointer++] = o;
     }
+
+    /**
+     * Gets and removes the most recent object pushed onto the stack.
+     *
+     * @return the most recent object pushed to the stack
+     * @throws EmptyStackException if the stack is empty
+     */
     public T pop() {
-	if(pointer <= 0)
-	    throw new EmptyStackException();
-	return objects[--pointer];
+        if (pointer <= 0)
+            throw new EmptyStackException();
+        return objects[--pointer];
     }
+
+    /**
+     * Checks if the stack is empty.
+     *
+     * @return true if there are no items in the stack, false otherwise
+     */
     public boolean isEmpty() {
-	return pointer <= 0;
-    } 
+        return pointer <= 0;
+    }
 }
