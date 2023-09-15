ff8e79e4b4748285af7df0c94e4a4825a54e88aa
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-09-15 15:23:26 -0700

    fixed small formatting things

--- experiment/generator/toga.sh
+++ experiment/generator/toga.sh
@@ -7,7 +7,7 @@ set -e
 
 # argument and setup check
 if [ ! $# -eq 3 ]; then
-  echo -e "(TOGA) Incorrect number of arguments. Expected 3 arguments, but got $#".
+  echo -e "(TOGA) Incorrect number of arguments. Expected 3 arguments, but got ${#}".
   exit 1
 fi
 
@@ -18,9 +18,8 @@ else
 fi
 
 ROOT_DIR=$(pwd)
-FULLY_QUALIFIED_NAME="$1"
-SRC_PATH="$2"
-EVOSUITE_OUTPUT="$3"
+FULLY_QUALIFIED_NAME="${1}"
+SRC_PATH="${2}"
 TOGA_PROJECT_DIR="${ROOT_DIR}${SEPARATOR}generator${SEPARATOR}resources${SEPARATOR}toga"
 TOGA_INPUT_DIR="${ROOT_DIR}${SEPARATOR}output${SEPARATOR}toga${SEPARATOR}input"
 TOGA_OUTPUT_DIR="${ROOT_DIR}${SEPARATOR}output${SEPARATOR}toga${SEPARATOR}output"
@@ -28,27 +27,23 @@ TOGA_OUTPUT_DIR="${ROOT_DIR}${SEPARATOR}output${SEPARATOR}toga${SEPARATOR}output
 echo "[2] Setup TOGA project"
 bash ".${SEPARATOR}generator${SEPARATOR}utils${SEPARATOR}toga_setup.sh"
 
-echo "[3] Generate test prefixes from EvoSuite test suite."
-java -jar "generator${SEPARATOR}resources${SEPARATOR}experiment.jar" toga remove_oracles "$EVOSUITE_OUTPUT" "$FULLY_QUALIFIED_NAME"
 echo "[4] Generate TOGA input files."
-java -jar "generator${SEPARATOR}resources${SEPARATOR}experiment.jar" toga generate_tog_inputs "$SRC_PATH" "$FULLY_QUALIFIED_NAME"
+java -jar "generator${SEPARATOR}resources${SEPARATOR}experiment.jar" "generate_tog_input" "toga" "${FULLY_QUALIFIED_NAME}" "${SRC_PATH}"
 
 echo "[5] Generate oracles with TOGA."
-cd "$TOGA_PROJECT_DIR"
+cd "${TOGA_PROJECT_DIR}"
 
 python3 toga.py "${TOGA_INPUT_DIR}${SEPARATOR}toga_input.csv" "${TOGA_INPUT_DIR}${SEPARATOR}toga_metadata.csv"
 
-cd "$ROOT_DIR"
+cd "${ROOT_DIR}"
 
-if [ ! -d "$TOGA_OUTPUT_DIR" ]; then
+if [ ! -d "${TOGA_OUTPUT_DIR}" ]; then
     # If it doesn't exist, create the folder
-    mkdir -p "$TOGA_OUTPUT_DIR"
-    echo "Folder created: $TOGA_OUTPUT_DIR"
+    mkdir -p "${TOGA_OUTPUT_DIR}"
+    echo "Folder created: ${TOGA_OUTPUT_DIR}"
 fi
 
 mv "${TOGA_PROJECT_DIR}${SEPARATOR}oracle_preds.csv" "${TOGA_OUTPUT_DIR}"
 
 echo "[6] Map oracles generated with TOGA into OracleOutputs."
-java -jar "generator${SEPARATOR}resources${SEPARATOR}experiment.jar" toga generate_oracle_outputs "$SRC_PATH"
-
-echo "[7] Insert oracles on test prefixes."
+java -jar "generator${SEPARATOR}resources${SEPARATOR}experiment.jar" "generate_oracle_output" "toga" "${SRC_PATH}"
