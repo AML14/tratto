266ad8f904c10f3cb24e3ef874281a4c1147a9ed
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-09-17 13:27:27 -0700

    added curly brackets and reverted runner script

--- experiment/runner.sh
+++ experiment/runner.sh
@@ -6,21 +6,21 @@
 JDK8_NAME="jdk-1.8.jdk"
 
 # get global path variables
-ROOT_DIR="$(dirname "$(realpath "$0")")"
-RESOURCES_DIR="$ROOT_DIR/generator/resources"
-OUTPUT_DIR="$ROOT_DIR/output"
-JAVA8_HOME="$RESOURCES_DIR/$JDK8_NAME/Contents/Home"
-JAVA8_BIN="$JAVA8_HOME/bin/java"
-JAVA8_C="$JAVA8_HOME/bin/javac"
+ROOT_DIR="$(dirname "$(realpath "${0}")")"
+RESOURCES_DIR="${ROOT_DIR}/generator/resources"
+OUTPUT_DIR="${ROOT_DIR}/output"
+JAVA8_HOME="${RESOURCES_DIR}/${JDK8_NAME}/Contents/Home"
+JAVA8_BIN="${JAVA8_HOME}/bin/java"
+JAVA8_C="${JAVA8_HOME}/bin/javac"
 # get given variables
-TOG=$1
-TARGET_CLASS=$2
-SRC_DIR=$(realpath "$3")
-BIN_DIR=$(realpath "$4")
-TEST_DIR="$OUTPUT_DIR/tog-tests/$TOG"
+TOG="jdoctor"
+TARGET_CLASS=${2}
+SRC_DIR=$(realpath "${3}")
+BIN_DIR=$(realpath "${4}")
+TEST_DIR="${OUTPUT_DIR}/tog-tests/${TOG}"
 # get project root directory
-IFS='/' read -ra SRC_ARR <<< "$SRC_DIR"
-IFS='/' read -ra BIN_ARR <<< "$BIN_DIR"
+IFS='/' read -ra SRC_ARR <<< "${SRC_DIR}"
+IFS='/' read -ra BIN_ARR <<< "${BIN_DIR}"
 PROJECT_DIR=""
 for i in "${!SRC_ARR[@]}" "${!BIN_ARR[@]}"; do
   if [ "${SRC_ARR[i]}" != "${BIN_ARR[i]}" ]; then
@@ -31,46 +31,46 @@ done
 PROJECT_DIR="${PROJECT_DIR#/}"
 
 # copy tests and utilities into target project
-cp -r "$TEST_DIR" "$PROJECT_DIR/evosuite-tests"
-cp "$RESOURCES_DIR/evosuite-1.0.6.jar" "$PROJECT_DIR"
-cp "$RESOURCES_DIR/evosuite-standalone-runtime-1.0.6.jar" "$PROJECT_DIR"
-cd "$PROJECT_DIR" || exit 1
+cp -r "${TEST_DIR}" "${PROJECT_DIR}/evosuite-tests"
+cp "${RESOURCES_DIR}/evosuite-1.0.6.jar" "${PROJECT_DIR}"
+cp "${RESOURCES_DIR}/evosuite-standalone-runtime-1.0.6.jar" "${PROJECT_DIR}"
+cd "${PROJECT_DIR}" || exit 1
 
 # run tests
-(export JAVA_HOME=$JAVA8_HOME;
+(export JAVA_HOME=${JAVA8_HOME};
 mvn dependency:copy-dependencies
 export CLASSPATH=target/classes:evosuite-standalone-runtime-1.0.6.jar:evosuite-tests:target/dependency/junit-4.12.jar:target/dependency/hamcrest-core-1.3.jar
 # compile all tests
-find "$PROJECT_DIR/evosuite-tests" -type f -name "*.java" > java_tests.txt
+find "${PROJECT_DIR}/evosuite-tests" -type f -name "*.java" > java_tests.txt
 while read -r java_test; do
-  $JAVA8_C "$java_test"
+  $JAVA8_C "${java_test}"
 done < java_tests.txt
 # run JUnit
 TEST_FAILS="test_fails.txt"
 while read -r java_test; do
-  test_class="${java_test#$PROJECT_DIR/evosuite-tests/}"  # remove project prefix
+  test_class="${java_test#${PROJECT_DIR}/evosuite-tests/}"  # remove project prefix
   test_class="${test_class%.java}"  # remove java suffix
   test_class="${test_class//\//.}"  # convert to package name
   # do not run scaffolding files
-  if [[ "$test_class" == *ESTest ]]; then
-    junit_output=$($JAVA8_BIN org.junit.runner.JUnitCore "$test_class")
-    if [[ $junit_output == *"FAILURES!!!"* ]]; then
+  if [[ "${test_class}" == *ESTest ]]; then
+    junit_output=$($JAVA8_BIN org.junit.runner.JUnitCore "${test_class}")
+    if [[ ${junit_output} == *"FAILURES!!!"* ]]; then
       # get names of all failing tests
       while IFS= read -r line; do
         regex="^[0-9]+\) test[0-9]+\(.*\)$"
-        if [[ "$line" =~ $regex ]]; then
-          echo "$line" >> "$TEST_FAILS"
+        if [[ "${line}" =~ ${regex} ]]; then
+          echo "${line}" >> "${TEST_FAILS}"
         fi
-      done <<< "$junit_output"
+      done <<< "${junit_output}"
     fi
   fi
 done < java_tests.txt;
-mv "$TEST_FAILS" "$OUTPUT_DIR/tog-tests/$TOG"
+mv "${TEST_FAILS}" "${OUTPUT_DIR}/tog-tests/${TOG}"
 )
 
 # generate TestOutput record
-EXPERIMENT="java -jar $RESOURCES_DIR/experiment.jar"
-$EXPERIMENT "$TOG" "generate_test_output" "$TARGET_CLASS" "$SRC_DIR" "$BIN_DIR" "$ROOT_DIR"
+EXPERIMENT="java -jar ${RESOURCES_DIR}/experiment.jar"
+$EXPERIMENT "${TOG}" "generate_test_output" "${TARGET_CLASS}" "${SRC_DIR}" "${BIN_DIR}" "${ROOT_DIR}"
 
 # cleanup
 rm -f "java_tests.txt"
