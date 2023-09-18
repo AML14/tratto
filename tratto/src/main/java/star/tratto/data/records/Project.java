6d52821665a5edfeb5023d8d5ae07db4a86d98e9
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-09-18 10:50:43 -0700

    modified Project to match other record conventions

--- tratto/src/main/java/star/tratto/data/records/Project.java
+++ tratto/src/main/java/star/tratto/data/records/Project.java
@@ -5,17 +5,16 @@ import java.nio.file.Path;
 /**
  * This record stores the basic file I/O information for a project in the
  * resources directory.
- *
- * @param projectName the name of the Java project
- * @param rootPath the project root directory
- * @param jarPath the directory with the project jar file
- * @param conditionsPath the directory of JDoctor conditions for the project
- * @param srcPath the project source code directory
  */
 public record Project(
+        /* The name of the Java project. */
         String projectName,
+        /* The project root directory. */
         Path rootPath,
+        /* The directory with the project JAR file. */
         Path jarPath,
+        /* The directory of JDoctor conditions for the project.  */
         Path conditionsPath,
+        /* The project source code directory. */
         Path srcPath
 ) {}
