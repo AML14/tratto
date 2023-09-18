8e86c3e74ce132a7df193576d515ac44b285d87e
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-09-18 08:51:36 -0700

    fixed toga incompatibility issues

--- experiment/generator/toga.sh
+++ experiment/generator/toga.sh
@@ -4,32 +4,39 @@
 
 # Exit from the program if any error is arose from another bash script or another command executed within this bash script.
 set -e
+if [[ $(uname) == "Darwin" || $(uname) == "Linux" ]]; then
+  export SEPARATOR="/"
+else
+  export SEPARATOR="\\"
+fi
 
 FULLY_QUALIFIED_NAME="${1}"
 SRC_PATH="${2}"
 
 # argument and setup check
-if [ ! $# -eq 3 ]; then
-  echo -e "(TOGA) Incorrect number of arguments. Expected 3 arguments, but got ${#}".
+if [ ! $# -eq 2 ]; then
+  echo -e "toga.sh: Incorrect number of arguments. Expected 3 arguments, but got ${#}".
   exit 1
 fi
 
 # Set and check environment variables
-SCRIPTDIR="$(cd "$(dirname "$0")" && pwd -P)"
-. "${SCRIPTDIR}${SEPARATOR}utils${SEPARATOR}env.sh"
+SCRIPT_DIR="$(cd "$(dirname "${0}")" && pwd -P)"
+# shellcheck source=generator/utils/env.sh
+. "${SCRIPT_DIR}${SEPARATOR}utils${SEPARATOR}env.sh"
 
-ROOT_DIR=$(pwd)
+ROOT_DIR="$(dirname "${SCRIPT_DIR}")"
+RESOURCES_DIR="${ROOT_DIR}${SEPARATOR}generator${SEPARATOR}resources"
 TOGA_PROJECT_DIR="${ROOT_DIR}${SEPARATOR}generator${SEPARATOR}resources${SEPARATOR}toga"
 TOGA_INPUT_DIR="${ROOT_DIR}${SEPARATOR}output${SEPARATOR}toga${SEPARATOR}input"
 TOGA_OUTPUT_DIR="${ROOT_DIR}${SEPARATOR}output${SEPARATOR}toga${SEPARATOR}output"
 
-echo "(TOGA) Setup TOGA project"
-bash ".${SEPARATOR}generator${SEPARATOR}utils${SEPARATOR}toga_setup.sh"
+echo "toga.sh: Setup TOGA project"
+bash "${ROOT_DIR}${SEPARATOR}generator${SEPARATOR}utils${SEPARATOR}toga_setup.sh"
 
-echo "(TOGA) Generate TOGA input files."
-java -jar "generator${SEPARATOR}resources${SEPARATOR}experiment.jar" "generate_tog_input" "toga" "${FULLY_QUALIFIED_NAME}" "${SRC_PATH}"
+echo "toga.sh: Generate TOGA input files."
+java -jar "${RESOURCES_DIR}${SEPARATOR}experiment.jar" "generate_tog_input" "toga" "${FULLY_QUALIFIED_NAME}" "${SRC_PATH}"
 
-echo "(TOGA) Generate oracles with TOGA."
+echo "toga.sh: Generate oracles with TOGA."
 cd "${TOGA_PROJECT_DIR}"
 
 python3 toga.py "${TOGA_INPUT_DIR}${SEPARATOR}toga_input.csv" "${TOGA_INPUT_DIR}${SEPARATOR}toga_metadata.csv"
@@ -44,5 +51,5 @@ fi
 
 mv "${TOGA_PROJECT_DIR}${SEPARATOR}oracle_preds.csv" "${TOGA_OUTPUT_DIR}"
 
-echo "(TOGA) Map oracles generated with TOGA into OracleOutputs."
-java -jar "generator${SEPARATOR}resources${SEPARATOR}experiment.jar" "generate_oracle_output" "toga" "${SRC_PATH}"
+echo "toga.sh: Map oracles generated with TOGA into OracleOutputs."
+java -jar "${RESOURCES_DIR}${SEPARATOR}experiment.jar" "generate_oracle_output" "toga" "${SRC_PATH}"
