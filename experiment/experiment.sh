80f6db39c768d34a8ccbbf9fdccb8c8b8622c9ae
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-09-18 08:25:47 -0700

    fixed line breaks

--- experiment/experiment.sh
+++ experiment/experiment.sh
@@ -15,7 +15,6 @@
 
 # Exit from the program if any error is arose from another bash script or another command executed within this bash script.
 set -e
-
 if [[ $(uname) == "Darwin" || $(uname) == "Linux" ]]; then
     SEPARATOR="/"
 else
