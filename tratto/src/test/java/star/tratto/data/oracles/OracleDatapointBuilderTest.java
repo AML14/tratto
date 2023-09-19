8f4f694a689d1a16ded54377fefce8aec5bfa46e
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-09-19 08:50:35 -0700

    modified OracleDatapointBuilderTest to use new enum

--- tratto/src/test/java/star/tratto/data/oracles/OracleDatapointBuilderTest.java
+++ tratto/src/test/java/star/tratto/data/oracles/OracleDatapointBuilderTest.java
@@ -1,6 +1,5 @@
 package star.tratto.data.oracles;
 
-import org.javatuples.Triplet;
 import org.junit.jupiter.api.Test;
 import star.tratto.data.OracleDatapoint;
 import star.tratto.data.records.MethodArgumentTokens;
@@ -25,34 +24,23 @@ public class OracleDatapointBuilderTest {
         // test project-level build.
         builder.setProjectName("myProject");
         builder.setClassName("myClass");
-        builder.build("project");
+        builder.build(FeatureLevel.PROJECT);
         assertEquals("myProject", builder.copy().getProjectName());
         assertNull(builder.copy().getClassName());
         // test class-level build.
         builder.setClassName("myClass");
         builder.setTokensMethodArguments(List.of(new MethodArgumentTokens("o1", "java.lang", "Object")));
-        builder.build("class");
+        builder.build(FeatureLevel.CLASS);
         assertEquals("myProject", builder.copy().getProjectName());
         assertEquals("myClass", builder.copy().getClassName());
         assertNull(builder.copy().getTokensMethodArguments());
         // test method-level build.
         builder.setTokensMethodArguments(List.of(new MethodArgumentTokens("o1", "java.lang", "Object")));
         builder.setId(7);
-        builder.build("method");
+        builder.build(FeatureLevel.METHOD);
         assertEquals("myProject", builder.copy().getProjectName());
         assertEquals("myClass", builder.copy().getClassName());
         assertEquals(List.of(new MethodArgumentTokens("o1", "java.lang", "Object")), builder.copy().getTokensMethodArguments());
         assertNull(builder.copy().getId());
-    }
-
-    @Test
-    public void buildInvalidTest() {
-        OracleDatapointBuilder builder = new OracleDatapointBuilder();
-        try {
-            builder.build("not a level");
-            fail("Expected an error to occur.");
-        } catch (AssertionError e) {
-            assertNotNull(e);
-        }
     }
 }
