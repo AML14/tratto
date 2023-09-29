d3b7c20d10d16ba41939754d49bf0e47971797c7
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-09-29 12:20:58 -0700

    used curly brackets for uniform style

--- experiment/generator/evosuite.sh
+++ experiment/generator/evosuite.sh
@@ -4,33 +4,33 @@
 
 # Argument and setup check
 if [ ! $# -eq 2 ]; then
-  echo -e "evosuite.sh: Incorrect number of arguments. Expected 2 arguments, but got $#".
+  echo -e "evosuite.sh: Incorrect number of arguments. Expected 2 arguments, but got ${#}".
   exit 1
-elif [ ! -d "$2" ]; then
-  echo -e "evosuite.sh: The system binaries path \"$2\" does not exist."
+elif [ ! -d "${2}" ]; then
+  echo -e "evosuite.sh: The system binaries path \"${2}\" does not exist."
   exit 1
 fi
 
 # Get current directory
 # shellcheck disable=SC2128
-current_dir=$(realpath "$(dirname "$BASH_SOURCE")")
+current_dir=$(realpath "$(dirname "${BASH_SOURCE}")")
 # Setup global variables
 source "${current_dir}/utils/global_variables.sh"
 
 # Setup local variables
-target_class="$1"  # Fully-qualified name of target class
-target_dir="$2"    # Directory of binary files of the system under test
+target_class="${1}"  # Fully-qualified name of target class
+target_dir="${2}"    # Directory of binary files of the system under test
 
 # Setup sdkman
-source "${UTILS_DIR}/init_sdkman.sh" "$SDKMAN_DIR"
+source "${UTILS_DIR}/init_sdkman.sh" "${SDKMAN_DIR}"
 
 # Generate output dir if it does not exists
-if ! [ -d "$OUTPUT_DIR" ]; then
-  mkdir "$OUTPUT_DIR"
+if ! [ -d "${OUTPUT_DIR}" ]; then
+  mkdir "${OUTPUT_DIR}"
 fi
 
 # Switch to Java 8
-sdk use java "$JAVA8"
+sdk use java "${JAVA8}"
 
 # Generate tests using EvoSuite
-java -jar "$EVOSUITE_JAR" -class "$target_class" -projectCP "$target_dir" -base_dir="${OUTPUT_DIR}" -seed=42
+java -jar "${EVOSUITE_JAR}" -class "${target_class}" -projectCP "${target_dir}" -base_dir="${OUTPUT_DIR}" -seed=13042023
