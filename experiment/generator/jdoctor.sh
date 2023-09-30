86d8e6bb5921e711a8bab1da2dee4ef56668d8f6
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-09-29 18:29:35 -0700

    fixed whitespace and capitalization issues

--- experiment/generator/jdoctor.sh
+++ experiment/generator/jdoctor.sh
@@ -2,7 +2,7 @@
 # This script generates a list of axiomatic oracles using JDoctor. Saves the
 # output to "output/jdoctor/oracle" as a list of OracleOutput records.
 
-# Arguments check
+# arguments check
 if [ ! $# -eq 3 ]; then
   echo -e "jdoctor.sh: Expected 3 arguments, but got ${#}".
   exit 1
@@ -14,39 +14,37 @@ elif [ ! -d "${3}" ]; then
   exit 1
 fi
 
-# Get current directory
+# get current directory
 # shellcheck disable=SC2128
 current_dir=$(realpath "$(dirname "${BASH_SOURCE}")")
-# Setup global variables
+# setup global variables
 source "${current_dir}/utils/global_variables.sh"
 
-# Define local variables
+# define local variables
 target_class="${1}"  # fully-qualified name of target class
 src_dir="${2}"  # project source directory
 class_dir="${3}"  # path to binary files of the system under test
 
-# Setup sdkman
+# setup sdkman
 source "${UTILS_DIR}/init_sdkman.sh" "${SDKMAN_DIR}"
 
-# Generate output dir if it does not exists
+# generate output dir if it does not exists
 if ! [ -d "${OUTPUT_DIR}/jdoctor" ]; then
   mkdir -p "${OUTPUT_DIR}/jdoctor"
 fi
 
-# Switch to Java 8
+# switch to Java 8
 sdk use java "${JAVA8}"
 
-# Execute JDoctor to generate oracles
-(
-  java -jar "${JDOCTOR_JAR}" \
+# execute JDoctor to generate oracles
+java -jar "${JDOCTOR_JAR}" \
     --target-class "${target_class}" \
     --source-dir "${src_dir}" \
     --class-dir "${class_dir}" \
-    --condition-translator-output "${OUTPUT_DIR}/jdoctor/jdoctor_output.json"  # location of JDoctor output in JSON format
-)
+    --condition-translator-output "${OUTPUT_DIR}/jdoctor/jdoctor_output.json"
 
-# Switch to Java 17
+# switch to Java 17
 sdk use java "${JAVA17}"
 
-# Convert JDoctor JSON EXPERIMENT_JARto OracleOutput
+# convert JDoctor JSON EXPERIMENT_JARto OracleOutput
 java -jar "${}" "jdoctor" "generate_oracle_outputs" "${OUTPUT_DIR}/jdoctor/jdoctor_output.json"
