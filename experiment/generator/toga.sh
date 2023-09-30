a7f17926de19b488b7965a0d78e5175a9694ade4
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-09-29 18:59:12 -0700

    fixed experiment.jar invocations

--- experiment/generator/toga.sh
+++ experiment/generator/toga.sh
@@ -7,23 +7,26 @@
 current_dir=$(realpath "$(dirname "$BASH_SOURCE")")
 source "${current_dir}/utils/global_variables.sh"
 
+# argument and setup check
+if [ ! ${#} -eq 2 ]; then
+  echo -e "toga.sh: Incorrect number of arguments. Expected 2 arguments, but got ${#}".
+  exit 1
+fi
+
 # Define local variables
 fully_qualified_name="$1"
 src_path="$2"
-EVOSUITE_OUTPUT="$3"
 TOGA_PROJECT_DIR="${ROOT_DIR}/generator/resources/toga"
 TOGA_INPUT_DIR="${ROOT_DIR}/output/toga/input"
 TOGA_OUTPUT_DIR="${ROOT_DIR}/output/toga/output"
 
-echo "[2] Setup TOGA project"
-bash "./generator/utils/toga_setup.sh"
+echo "toga.sh: Setup TOGA project"
+bash "$ROOT_DIR/generator/utils/toga_setup.sh"
 
-echo "[3] Generate test prefixes from EvoSuite test suite."
-java -jar "generator/resources/experiment.jar" toga remove_oracles "$EVOSUITE_OUTPUT" "$fully_qualified_name"
-echo "[4] Generate TOGA input files."
-java -jar "generator/resources/experiment.jar" toga generate_tog_inputs "$src_path" "$fully_qualified_name"
+echo "toga.sh: Generate TOGA input files."
+java -jar "generator/resources/experiment.jar" "generate_tog_input" "toga" "$fully_qualified_name" "$src_path"
 
-echo "[5] Generate oracles with TOGA."
+echo "toga.sh: Generate oracles with TOGA."
 cd "$TOGA_PROJECT_DIR" || exit 1
 
 python3 toga.py "${TOGA_INPUT_DIR}/toga_input.csv" "${TOGA_INPUT_DIR}/toga_metadata.csv"
@@ -38,7 +41,5 @@ fi
 
 mv "${TOGA_PROJECT_DIR}/oracle_preds.csv" "${TOGA_OUTPUT_DIR}"
 
-echo "[6] Map oracles generated with TOGA into OracleOutputs."
-java -jar "generator/resources/experiment.jar" toga generate_oracle_outputs "$src_path"
-
-echo "[7] Insert oracles on test prefixes."
+echo "toga.sh: Map oracles generated with TOGA into OracleOutputs."
+java -jar "generator/resources/experiment.jar" "generate_oracle_output" "toga" "$src_path"
