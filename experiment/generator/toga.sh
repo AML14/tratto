b75ed0c990cf3328a105f0b5d824477884592c3f
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-09-29 19:04:27 -0700

    removed capslock for consistency

--- experiment/generator/toga.sh
+++ experiment/generator/toga.sh
@@ -16,9 +16,9 @@ fi
 # Define local variables
 fully_qualified_name="${1}"
 src_path="${2}"
-TOGA_PROJECT_DIR="${ROOT_DIR}/generator/resources/toga"
-TOGA_INPUT_DIR="${ROOT_DIR}/output/toga/input"
-TOGA_OUTPUT_DIR="${ROOT_DIR}/output/toga/output"
+toga_project_dir="${ROOT_DIR}/generator/resources/toga"
+toga_input_dir="${ROOT_DIR}/output/toga/input"
+toga_output_dir="${ROOT_DIR}/output/toga/output"
 
 echo "toga.sh: Setup TOGA project"
 bash "${ROOT_DIR}/generator/utils/toga_setup.sh"
@@ -27,19 +27,19 @@ echo "toga.sh: Generate TOGA input files."
 java -jar "generator/resources/experiment.jar" "generate_tog_input" "toga" "${fully_qualified_name}" "${src_path}"
 
 echo "toga.sh: Generate oracles with TOGA."
-cd "${TOGA_PROJECT_DIR}" || exit 1
+cd "${toga_project_dir}" || exit 1
 
-python3 toga.py "${TOGA_INPUT_DIR}/toga_input.csv" "${TOGA_INPUT_DIR}/toga_metadata.csv"
+python3 toga.py "${toga_input_dir}/toga_input.csv" "${toga_input_dir}/toga_metadata.csv"
 
 cd "${ROOT_DIR}" || exit 1
 
-if [ ! -d "${TOGA_OUTPUT_DIR}" ]; then
+if [ ! -d "${toga_output_dir}" ]; then
     # If it doesn't exist, create the folder
-    mkdir -p "${TOGA_OUTPUT_DIR}"
-    echo "Folder created: ${TOGA_OUTPUT_DIR}"
+    mkdir -p "${toga_output_dir}"
+    echo "Folder created: ${toga_output_dir}"
 fi
 
-mv "${TOGA_PROJECT_DIR}/oracle_preds.csv" "${TOGA_OUTPUT_DIR}"
+mv "${toga_project_dir}/oracle_preds.csv" "${toga_output_dir}"
 
 echo "toga.sh: Map oracles generated with TOGA into OracleOutputs."
 java -jar "generator/resources/experiment.jar" "generate_oracle_output" "toga" "${src_path}"
