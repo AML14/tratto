1cd74b747a1f278e2ddc2e82e391e426a8ed9649
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-09-29 18:28:44 -0700

    added curl brackets for consistency

--- experiment/generator/jdoctor.sh
+++ experiment/generator/jdoctor.sh
@@ -16,37 +16,37 @@ fi
 
 # Get current directory
 # shellcheck disable=SC2128
-current_dir=$(realpath "$(dirname "$BASH_SOURCE")")
+current_dir=$(realpath "$(dirname "${BASH_SOURCE}")")
 # Setup global variables
 source "${current_dir}/utils/global_variables.sh"
 
 # Define local variables
-target_class="$1"  # fully-qualified name of target class
-src_dir="$2"  # project source directory
-class_dir="$3"  # path to binary files of the system under test
+target_class="${1}"  # fully-qualified name of target class
+src_dir="${2}"  # project source directory
+class_dir="${3}"  # path to binary files of the system under test
 
 # Setup sdkman
-source "${UTILS_DIR}/init_sdkman.sh" "$SDKMAN_DIR"
+source "${UTILS_DIR}/init_sdkman.sh" "${SDKMAN_DIR}"
 
 # Generate output dir if it does not exists
-if ! [ -d "$OUTPUT_DIR/jdoctor" ]; then
-  mkdir -p "$OUTPUT_DIR/jdoctor"
+if ! [ -d "${OUTPUT_DIR}/jdoctor" ]; then
+  mkdir -p "${OUTPUT_DIR}/jdoctor"
 fi
 
 # Switch to Java 8
-sdk use java "$JAVA8"
+sdk use java "${JAVA8}"
 
 # Execute JDoctor to generate oracles
 (
-  java -jar "$JDOCTOR_JAR" \
-    --target-class "$target_class" \
-    --source-dir "$src_dir" \
-    --class-dir "$class_dir" \
+  java -jar "${JDOCTOR_JAR}" \
+    --target-class "${target_class}" \
+    --source-dir "${src_dir}" \
+    --class-dir "${class_dir}" \
     --condition-translator-output "${OUTPUT_DIR}/jdoctor/jdoctor_output.json"  # location of JDoctor output in JSON format
 )
 
 # Switch to Java 17
-sdk use java "$JAVA17"
+sdk use java "${JAVA17}"
 
-# Convert JDoctor JSON to OracleOutput
-java -jar "$EXPERIMENT_JAR" "jdoctor" "generate_oracle_outputs" "${OUTPUT_DIR}/jdoctor/jdoctor_output.json"
+# Convert JDoctor JSON EXPERIMENT_JARto OracleOutput
+java -jar "${}" "jdoctor" "generate_oracle_outputs" "${OUTPUT_DIR}/jdoctor/jdoctor_output.json"
