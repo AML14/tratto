83bc81e202a2e487d51e8ee01e1adebc453aa3c5
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-09-29 17:52:02 -0700

    uncapitalized for uniformity

--- experiment/generator/evosuite.sh
+++ experiment/generator/evosuite.sh
@@ -2,7 +2,7 @@
 # This script generates a test suite using EvoSuite and saves the output to
 # "output/evosuite-tests".
 
-# Argument and setup check
+# argument and setup check
 if [ ! $# -eq 2 ]; then
   echo -e "evosuite.sh: Incorrect number of arguments. Expected 2 arguments, but got ${#}".
   exit 1
@@ -11,28 +11,28 @@ elif [ ! -d "${2}" ]; then
   exit 1
 fi
 
-# Get current directory
+# get current directory
 # shellcheck disable=SC2128
 current_dir=$(realpath "$(dirname "${BASH_SOURCE}")")
-# Setup global variables
+# setup global variables
 source "${current_dir}/utils/global_variables.sh"
 
-# Setup local variables
+# setup local variables
 target_class="${1}"  # Fully-qualified name of target class
 target_dir="${2}"    # Directory of binary files of the system under test
 
-# Setup sdkman
+# setup sdkman
 source "${UTILS_DIR}/init_sdkman.sh" "${SDKMAN_DIR}"
 
-# Generate output dir if it does not exists
+# generate output dir if it does not exists
 if ! [ -d "${OUTPUT_DIR}" ]; then
   mkdir "${OUTPUT_DIR}"
 fi
 
-# Switch to Java 8
+# switch to Java 8
 sdk use java "${JAVA8}"
 
-# Generate tests using EvoSuite
+# generate tests using EvoSuite
 java -jar "${EVOSUITE_JAR}" -class "${target_class}" -projectCP "${target_dir}" -seed 13042023
 rm -r "${ROOT_DIR}/evosuite-report"  # delete statistics
 # overwrites previous output if necessary
