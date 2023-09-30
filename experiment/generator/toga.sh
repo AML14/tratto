01b3c126c4af2b8d59e6c2d37873f71dea713917
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-09-29 19:01:18 -0700

    added curly brackets for consistency

--- experiment/generator/toga.sh
+++ experiment/generator/toga.sh
@@ -4,7 +4,7 @@
 
 # Get current directory
 # shellcheck disable=SC2128
-current_dir=$(realpath "$(dirname "$BASH_SOURCE")")
+current_dir=$(realpath "$(dirname "${BASH_SOURCE}")")
 source "${current_dir}/utils/global_variables.sh"
 
 # argument and setup check
@@ -14,32 +14,32 @@ if [ ! ${#} -eq 2 ]; then
 fi
 
 # Define local variables
-fully_qualified_name="$1"
-src_path="$2"
+fully_qualified_name="${1}"
+src_path="${2}"
 TOGA_PROJECT_DIR="${ROOT_DIR}/generator/resources/toga"
 TOGA_INPUT_DIR="${ROOT_DIR}/output/toga/input"
 TOGA_OUTPUT_DIR="${ROOT_DIR}/output/toga/output"
 
 echo "toga.sh: Setup TOGA project"
-bash "$ROOT_DIR/generator/utils/toga_setup.sh"
+bash "${ROOT_DIR}/generator/utils/toga_setup.sh"
 
 echo "toga.sh: Generate TOGA input files."
-java -jar "generator/resources/experiment.jar" "generate_tog_input" "toga" "$fully_qualified_name" "$src_path"
+java -jar "generator/resources/experiment.jar" "generate_tog_input" "toga" "${fully_qualified_name}" "${src_path}"
 
 echo "toga.sh: Generate oracles with TOGA."
-cd "$TOGA_PROJECT_DIR" || exit 1
+cd "${TOGA_PROJECT_DIR}" || exit 1
 
 python3 toga.py "${TOGA_INPUT_DIR}/toga_input.csv" "${TOGA_INPUT_DIR}/toga_metadata.csv"
 
-cd "$ROOT_DIR" || exit 1
+cd "${ROOT_DIR}" || exit 1
 
-if [ ! -d "$TOGA_OUTPUT_DIR" ]; then
+if [ ! -d "${TOGA_OUTPUT_DIR}" ]; then
     # If it doesn't exist, create the folder
-    mkdir -p "$TOGA_OUTPUT_DIR"
-    echo "Folder created: $TOGA_OUTPUT_DIR"
+    mkdir -p "${TOGA_OUTPUT_DIR}"
+    echo "Folder created: ${TOGA_OUTPUT_DIR}"
 fi
 
 mv "${TOGA_PROJECT_DIR}/oracle_preds.csv" "${TOGA_OUTPUT_DIR}"
 
 echo "toga.sh: Map oracles generated with TOGA into OracleOutputs."
-java -jar "generator/resources/experiment.jar" "generate_oracle_output" "toga" "$src_path"
+java -jar "generator/resources/experiment.jar" "generate_oracle_output" "toga" "${src_path}"
