aa82dfcf51e5f712d12c38afa18bb0ff59a46852
davide <davide.molinelli@mail.polimi.it>
2023-11-06 02:28:33 +0100

    Ready to deploy

--- data-augmentation/server/server.js
+++ data-augmentation/server/server.js
@@ -12,7 +12,21 @@ dotenv.config({ path: "./config/config.env" });
 db.connect();
 
 // Allow all origin
-app.use(cors());
+const allowedOrigins = ["http://localhost:5173","https://tratto-jdc.onrender.com"];
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (allowedOrigins.includes(origin) || !origin) {
+            // If the origin is in the allowedOrigins array or it's undefined (e.g., a same-origin request), allow the request
+            callback(null, true);
+        } else {
+            // If the origin is not in the allowedOrigins array, reject the request
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json({ limit: '10mb' }));
 
