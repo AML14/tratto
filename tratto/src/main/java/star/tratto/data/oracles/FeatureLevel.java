74731e0831ba70654efa615f54db843d823c7c55
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-11-22 09:39:17 -0800

    reformatted FeatureLevel javadoc

--- tratto/src/main/java/star/tratto/data/oracles/FeatureLevel.java
+++ tratto/src/main/java/star/tratto/data/oracles/FeatureLevel.java
@@ -1,31 +1,39 @@
 package star.tratto.data.oracles;
 
+import star.tratto.data.OracleDatapoint;
+
 /**
- * The scope of a feature in an {@link star.tratto.data.OracleDatapoint}. The
- * levels are defined as:
- * <ul>
- *     <li>DEFAULT: tokensGeneralGrammar,
- *     tokensGeneralValuesGlobalDictionary</li>
- *     <li>PROJECT: tokensProjectClasses,
- *     tokensProjectClassesNonPrivateStaticNonVoidMethods,
- *     tokensProjectClassesNonPrivateStaticAttributes</li>
- *     <li>CLASS: className, classSourceCode, classJavadoc, packageName</li>
- *     <li>METHOD: methodSourceCode, methodJavadoc, tokensMethodArguments
- *     tokensMethodVariablesNonPrivateNonStaticNonVoidMethods,
- *     tokensMethodVariablesNonPrivateNonStaticAttributes</li>
- *     <li>ORACLE: tokensOracleVariablesNonPrivateNonStaticNonVoidMethods,
- *     tokensOracleVariablesNonPrivateNonStaticAttributes</li>
- * </ul>
+ * The scope of a feature in an {@link OracleDatapoint}.
  */
 public enum FeatureLevel {
-    /** The feature is equal across all datapoints. */
+    /**
+     * The feature is equal across all datapoints. Includes features:
+     * tokensGeneralGrammar, tokensGeneralValuesGlobalDictionary.
+     */
     DEFAULT,
-    /** The feature is equal across all datapoints in the same project. */
+    /**
+     * The feature is equal across all datapoints in the same project.
+     * Includes features: tokensProjectClasses,
+     * tokensProjectClassesNonPrivateStaticNonVoidMethods,
+     * tokensProjectClassesNonPrivateStaticAttributes.
+     */
     PROJECT,
-    /** The feature is equal across all datapoints in the same class. */
+    /**
+     * The feature is equal across all datapoints in the same class. Includes
+     * features: className, classSourceCode, classJavadoc, packageName.
+     */
     CLASS,
-    /** The feature is equal across all datapoints in the same method. */
+    /**
+     * The feature is equal across all datapoints in the same method. Includes
+     * features: methodSourceCode, methodJavadoc, tokensMethodArguments,
+     * tokensMethodVariablesNonPrivateNonStaticNonVoidMethods,
+     * tokensMethodVariablesNonPrivateNonStaticAttributes.
+     */
     METHOD,
-    /** The feature is specific to its oracle (possibly not unique).  */
+    /**
+     * The feature is specific to its oracle. Includes features:
+     * tokensOracleVariablesNonPrivateNonStaticNonVoidMethods,
+     * tokensOracleVariablesNonPrivateNonStaticAttributes.
+     */
     ORACLE
 }
