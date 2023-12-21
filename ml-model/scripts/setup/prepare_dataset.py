917d2875508c918a704b14b472ad70393555760f
davide <davide.molinelli@mail.polimi.it>
2023-12-21 12:14:49 +0100

    oracles model + refactoring old models + improved scripts

---
+++ ml-model/scripts/setup/prepare_dataset.py
@@ -0,0 +1,47 @@
+import os
+import pandas as pd
+
+d_paths = [
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "dataset", "original", "oracles-dataset-train"),
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "dataset", "original", "oracles-dataset-validation"),
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "dataset", "original", "token-classes-dataset-train"),
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "dataset", "original", "token-classes-dataset-validation"),
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "dataset", "original", "token-values-dataset-train"),
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "dataset", "original", "token-values-dataset-validation"),
+]
+c_paths = [
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "dataset", "cleaned", "oracles-dataset-train"),
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "dataset", "cleaned", "oracles-dataset-validation"),
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "dataset", "cleaned", "token-classes-dataset-train"),
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "dataset", "cleaned", "token-classes-dataset-validation"),
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "dataset", "cleaned", "token-values-dataset-train"),
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "dataset", "cleaned", "token-values-dataset-validation"),
+]
+
+for c_path in c_paths:
+    if not os.path.exists(c_path):
+        os.makedirs(os.path.join(c_path, 'json'))
+        os.makedirs(os.path.join(c_path, 'csv'))
+# list of partial dataframes
+dfs = []
+# collects partial dataframes from oracles
+for d_path, c_path in zip(d_paths, c_paths):
+    for file_name in os.listdir(d_path):
+        print(file_name)
+        df_dataset = pd.read_json(os.path.join(d_path,  file_name))
+        if len(df_dataset) > 0:
+            if d_path.endswith("oracles-dataset-train") or d_path.endswith("oracles-dataset-validation"):
+                # Map id into oracleId
+                df_dataset.rename(columns={'id': 'oracleId'}, inplace=True)
+                df_dataset.rename(columns={'id': 'oracleId'}, inplace=True)
+            else:
+                # drop column id (it is not relevant for training the model)
+                df_dataset = df_dataset.drop(['id'], axis=1)
+            # map empty cells to empty strings
+            df_dataset.fillna('', inplace=True)
+            # Remove method source code (keep only signature)
+            #df_dataset['methodSourceCode'] = df_dataset['methodSourceCode'].str.split('{').str[0]
+            # Delete the tgt labels from the input dataset, and others less relevant columns
+            df_dataset = df_dataset.drop(['projectName', 'classJavadoc', 'classSourceCode'], axis=1)
+            df_dataset.to_json(os.path.join(c_path, 'json', file_name))
+            df_dataset.to_csv(os.path.join(c_path, 'csv', file_name.replace(".json", ".csv")))
---
+++ ml-model/train_model.sh
@@ -0,0 +1,58 @@
+export PYTHONPATH="./"
+
+model_type="codet5+"
+tokenizer_name="Salesforce/codet5p-770m"
+model_name_or_path="Salesforce/codet5p-770m"
+tratto_model_type="oracles"
+transformer_type="decoder"
+task_name="oraclesModel_classifier_decoder"
+max_seq_length=512
+batch_size=16
+learning_rate=1e-5
+num_epochs=5
+checkpoint_steps=500
+accumulation_steps=1
+train_path="./dataset/cleaned/oracles-dataset-train/csv"
+validation_path="./dataset/cleaned/oracles-dataset-validation/csv"
+output_dir="./output_oracles_model_decoder_label_770"
+classification_type="label_prediction"
+
+if [ "$#" -eq 1 ] && [ "$1" == "--accelerate" ]; then
+  echo "Running with accelerate"
+  accelerate launch --config_file accelerate_config_fsdp.yaml scripts/train/run_classifier_accelerate.py \
+    --model_type "$model_type" \
+    --tokenizer_name "$tokenizer_name" \
+    --model_name_or_path "$model_name_or_path" \
+    --tratto_model_type "$tratto_model_type" \
+    --transformer_type "$transformer_type" \
+    --task_name "$task_name" \
+    --max_seq_length "$max_seq_length" \
+    --batch_size "$batch_size" \
+    --learning_rate "$learning_rate" \
+    --num_epochs "$num_epochs" \
+    --checkpoint_steps "$checkpoint_steps" \
+    --accumulation_steps "$accumulation_steps" \
+    --train_path "$train_path" \
+    --validation_path "$validation_path" \
+    --output_dir "$output_dir" \
+    --classification_type "$classification_type"
+else
+  echo "Running without accelerate"
+  python3 scripts/train/run_classifier.py \
+    --model_type "$model_type" \
+    --tokenizer_name "$tokenizer_name" \
+    --model_name_or_path "$model_name_or_path" \
+    --tratto_model_type "$tratto_model_type" \
+    --transformer_type "$transformer_type" \
+    --task_name "$task_name" \
+    --max_seq_length "$max_seq_length" \
+    --batch_size "$batch_size" \
+    --learning_rate "$learning_rate" \
+    --num_epochs "$num_epochs" \
+    --checkpoint_steps "$checkpoint_steps" \
+    --accumulation_steps "$accumulation_steps" \
+    --train_path "$train_path" \
+    --validation_path "$validation_path" \
+    --output_dir "$output_dir" \
+    --classification_type "$classification_type"
+fi
