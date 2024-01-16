46c7024adb3b6afccd75c1acd7bd9504f5c514bb
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2024-01-16 09:19:50 -0800

    outlined project iteration for generating oracles

--- experiment/generator/tog.sh
+++ experiment/generator/tog.sh
@@ -1,24 +1,41 @@
 #!/bin/bash
+tog=${1}
 # This script generates oracles for all bugs in Defects4J for a given TOG.
 current_dir=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
-source "${current_dir}/utils/global_variables.sh"
-source "${UTILS_DIR}/init_sdkman.sh"
-sdk use java "${JAVA8}"
+root_dir=$(dirname "${current_dir}")
+# setup defects4j and sdkman
+export PATH=$PATH:"${DEFECTS4J_HOME}"/framework/bin
+source "${current_dir}/utils/init_sdkman.sh"
 
-# Switch to Java 17
-#sdk use java "${JAVA8}"
-#
-## Generate oracles using TOG
-#if [ "${tog}" == "jdoctor" ]; then
-#  bash ./generator/jdoctor.sh "${target_class}" "${src_dir}" "${bin_dir}"
-#  oracle_output="${ROOT_DIR}/output/jdoctor/oracle_outputs.json"
-#elif [ "${tog}" == "toga" ]; then
-#  bash ./generator/toga.sh "${target_class}" "${src_dir}" "${evosuite_output}"
-#  oracle_output="${ROOT_DIR}/output/toga/oracle/oracle_outputs.json"
-#elif [ "${tog}" == "tratto" ]; then
-#  bash ./generator/tratto.sh "${target_class}" "${src_dir}" "${project_jar}"
-#  oracle_output="${ROOT_DIR}/output/tratto/oracle/oracle_outputs.json"
-#fi
-#cp "${oracle_output}" "${ROOT_DIR}/output/${tog}-oracles.json"
-## insert oracles into EvoSuite prefixes
-#java -jar "${EXPERIMENT_JAR}" "insert_oracles" "${tog}" "${bin_dir}" "${oracle_output}"
+sdk use java "8.0.382-amzn"
+while IFS=, read -r project_id bug_id modified_class; do
+  # check for a given project or bug id
+  if [ ${#} -gt 1 ]; then
+    if [ "${project_id}" != "${2}" ]; then
+      continue
+    fi
+    if [ ${#} -gt 2 ]; then
+      if [ "${bug_id}" != "${3}" ]; then
+        continue
+      fi
+    fi
+  fi
+  # checkout Defects4J project and export features
+  project_dir="${root_dir}/temp/${project_id}_${bug_id}"
+  defects4j checkout -p "${project_id}" -v "${bug_id}b" -w "${project_dir}"
+  classpath=$(defects4j export -p "cp.compile" -w "${project_dir}")
+  src_dir="${project_dir}/$(defects4j export -p "dir.src.classes" -w "${project_dir}")"
+  # generate oracles using TOG
+  if [ "${tog}" == "jdoctor" ]; then
+#    ./jdoctor.sh "${modified_class}" "${src_dir}" "${classpath}"
+    echo "${src_dir}"
+  elif [ "${tog}" == "toga" ]; then
+    ./toga.sh "${modified_class}"
+  elif [ "${tog}" == "tratto" ]; then
+    ./tratto.sh
+  else
+    echo -e "tog.sh: Invalid TOG name, ${tog}"
+    exit 1
+  fi
+  rm -r "${root_dir}/temp"
+done < "${current_dir}/resources/modified_classes.csv"
