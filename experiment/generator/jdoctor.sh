e24e8e124aa67ccb7e5fabf9d194ea80654bb4c3
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2024-01-16 09:37:02 -0800

    added JDoctor jar invocation

--- experiment/generator/jdoctor.sh
+++ experiment/generator/jdoctor.sh
@@ -1,53 +1,22 @@
 #!/bin/bash
 # This script generates a list of axiomatic oracles using JDoctor. Saves the
-# output to "output/jdoctor/oracle" as a list of OracleOutput records.
-
-# arguments check
-if [ ! $# -eq 3 ]; then
-  echo -e "jdoctor.sh: Expected 3 arguments, but got ${#}".
-  exit 1
-elif [ ! -d "${2}" ]; then
-  echo -e "jdoctor.sh: The source directory \"${2}\" does not exist."
-  exit 1
-elif [ ! -f "${3}" ]; then
-  echo -e "jdoctor.sh: The project jar \"${3}\" does not exist."
-  exit 1
-fi
-
-# get current directory
-current_dir=$(realpath "$(dirname "${BASH_SOURCE[@]}")")
-# setup global variables
-source "${current_dir}/utils/global_variables.sh"
-
-# define local variables
-target_class="${1}"  # fully-qualified name of target class
-src_dir="${2}"  # project source directory
-jar_path="${3}"  # path to jar of the project under test
-
+# output to "output/jdoctor-oracles" as a list of OracleOutput records.
+current_dir=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
+root_dir=$(dirname "${current_dir}")
 # setup sdkman
-source "${UTILS_DIR}/init_sdkman.sh" "${SDKMAN_DIR}"
-
-# switch to Java 8
-sdk use java "${JAVA8}"
-
-echo "jdoctor.sh: Setup JDoctor project"
-bash "${ROOT_DIR}/generator/utils/jdoctor_setup.sh"
-
-# generate output dir if it does not exists
-if ! [ -d "${OUTPUT_DIR}/jdoctor" ]; then
-  mkdir -p "${OUTPUT_DIR}/jdoctor"
+source "${current_dir}/utils/init_sdkman.sh"
+sdk use java "8.0.382-amzn"
+# check arguments
+if [ ! $# -eq 3 ]; then
+  echo -e "jdoctor.sh: Expected 3 arguments (target_class, src_dir, classpath), but got $#"
+  exit 1
 fi
+target_class="${1}"
+src_dir="${2}"
+classpath="${3}"
 
-# execute JDoctor to generate oracles
-mkdir -p "${OUTPUT_DIR}/jdoctor/output"
-java -jar "${JDOCTOR_JAR}" \
-    --target-class "${target_class}" \
-    --source-dir "${src_dir}" \
-    --class-dir "${jar_path}:${dependencies_path}" \
-    --condition-translator-output "${OUTPUT_DIR}/jdoctor/output/jdoctor_output.json"
-
-# switch to Java 17
-sdk use java "${JAVA17}"
-
-# convert JDoctor JSON to OracleOutput
-java -jar "${EXPERIMENT_JAR}" "generate_oracle_output" "jdoctor" ""   # empty argument for compatibility
+java -jar "${current_dir}/resources/toradocu-1.0-all.jar" \
+  --target-class "${target_class}" \
+  --source-dir "${src_dir}" \
+  --class-dir "${classpath}" \
+  --condition-translator-output "${root_dir}/output/jdoctor-oracles/${target_class}_jdoctor_output.json"
