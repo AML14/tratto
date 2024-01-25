c2afd99c4dcaadde11d39d6a82c410b04ed910dc
davide <davide.molinelli@mail.polimi.it>
2024-01-25 12:52:02 +0100

    open ai javadoctags generation - data augmentation, turn 3

--- ml-model/scripts/data_augmentation/batch_generator.py
+++ ml-model/scripts/data_augmentation/batch_generator.py
@@ -6,11 +6,11 @@ from src.utils import utils
 
 input_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "resources", "original_javadoctags.json")
 output_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "resources", "original_javadoctags_set.json")
-batch_size = 25
+batch_size = 15
 
 if __name__ == '__main__':
     _, javadoctags_list = utils.import_json(input_file_path)
-    javadoctags_list_batches = [javadoctags_list[i:i + 25] for i in range(0, len(javadoctags_list), 25)]
+    javadoctags_list_batches = [javadoctags_list[i:i + batch_size] for i in range(0, len(javadoctags_list), batch_size)]
     with open(output_file_path, "w") as f:
         json.dump(javadoctags_list_batches, f, indent=4)
 
