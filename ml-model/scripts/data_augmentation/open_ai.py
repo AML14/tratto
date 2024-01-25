bed20fe2d1f1073f878b7d66d2b44403dace8473
davide <davide.molinelli@mail.polimi.it>
2024-01-25 14:58:33 +0100

    updated open ai scripts

--- ml-model/scripts/data_augmentation/open_ai.py
+++ ml-model/scripts/data_augmentation/open_ai.py
@@ -31,40 +31,39 @@ def num_tokens_from_string(string: str, encoding_name: str) -> int:
 if __name__ == '__main__':
     # Iterate over the sets of original javadoc tags
     for idx, original_javadoctags in enumerate(original_javadoctags_set):
-        if idx in [1, 7, 14]:
-            # Generate list of messages as input to OpenAI request
-            messages = [
-                {
-                    "role": "user",
-                    "content": preface["user"]
-                },
-                {
-                    "role": "assistant",
-                    "content": preface["assistant"]
-                },
-                {
-                    "role": "user",
-                    "content": f"input={original_javadoctags}"
-                }
-            ]
+        # Generate list of messages as input to OpenAI request
+        messages = [
+            {
+                "role": "user",
+                "content": preface["user"]
+            },
+            {
+                "role": "assistant",
+                "content": preface["assistant"]
+            },
+            {
+                "role": "user",
+                "content": f"input={original_javadoctags}"
+            }
+        ]
 
-            # Compute total number of tokens as input to the OpenAI request (to monitor costs)
-            num_tokens = 0
-            for msg in messages:
-                num_tokens += num_tokens_from_string(msg["content"], "gpt-3.5-turbo-instruct")
-            print(f"Processing batch {idx}/{len(original_javadoctags_set)} - Tokens: {num_tokens}")
-            try:
-                # Perform request to OpenAI and save response
-                response = openai.ChatCompletion.create(
-                    model="gpt-3.5-turbo",
-                    messages=messages
-                )
-                # Read content of the response
-                content = response["choices"][0]["message"]["content"]
-                # Save response
-                if not os.path.exists(output_path):
-                    os.makedirs(output_path)
-                utils.export_stats(os.path.join(output_path, f"output_{idx}.json"), content)
-            except:
-                print(
-                    f"Failed to process batch {idx}/{len(original_javadoctags_set)} - Tokens: {num_tokens} (502 Bad Gataway error)")
+        # Compute total number of tokens as input to the OpenAI request (to monitor costs)
+        num_tokens = 0
+        for msg in messages:
+            num_tokens += num_tokens_from_string(msg["content"], "gpt-3.5-turbo-instruct")
+        print(f"Processing batch {idx}/{len(original_javadoctags_set)} - Tokens: {num_tokens}")
+        try:
+            # Perform request to OpenAI and save response
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=messages
+            )
+            # Read content of the response
+            content = response["choices"][0]["message"]["content"]
+            # Save response
+            if not os.path.exists(output_path):
+                os.makedirs(output_path)
+            utils.export_stats(os.path.join(output_path, f"output_{idx}.json"), content)
+        except:
+            print(
+                f"Failed to process batch {idx}/{len(original_javadoctags_set)} - Tokens: {num_tokens} (502 Bad Gataway error)")
