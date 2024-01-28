dcad0d66249d96511ee4a01fb548335ad36d1104
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2024-01-28 13:02:33 -0800

    added runner script outline

---
+++ experiment/runner.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# This script generates oracles for all bugs in Defects4J for a given TOG.
+current_dir=$(realpath "${BASH_SOURCE[0]}")
+root_dir=$(dirname "${current_dir}")
+# setup defects4j and sdkman
+export PATH=$PATH:"${DEFECTS4J_HOME}"/framework/bin
+source "${root_dir}/generator/utils/init_sdkman.sh"
+tog=${1}
+
+if [ ! -d "${root_dir}/test-suite" ]; then
+  mkdir "${root_dir}/test-suite"
+fi
+while IFS=, read -r project_id bug_id _; do
+  # check for a given project or bug id
+  if [ ${#} -gt 1 ]; then
+    if [ "${project_id}" != "${2}" ]; then
+      continue
+    fi
+    if [ ${#} -gt 2 ]; then
+      if [ "${bug_id}" != "${3}" ]; then
+        continue
+      fi
+    fi
+  fi
+  sdk use java "8.0.382-amzn"
+  # compress test suite
+  cd "${root_dir}/output/${tog}-tests/${project_id}/${bug_id}" || exit 1
+  tar -cvjSf "${project_id}-${bug_id}b-evosuite.tar.bz2" .
+  mv "${project_id}-${bug_id}b-evosuite.tar.bz2" "${root_dir}/test-suite"
+  cd - || exit 1
+  # run bug detection
+  run_bug_detection.pl -p "${project_id}" -d "${root_dir}/test-suite" -o "${root_dir}/test-suite"
+  # cleanup
+  rm "${root_dir}/test-suite/${project_id}-${bug_id}b-evosuite.tar.bz2"
+#  sdk use java "17.0.8-oracle"
+done < "${root_dir}/generator/resources/modified_classes.csv"
