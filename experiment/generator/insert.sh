323a8662c58cf73ffedc38ab84129e45a4a09dc6
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2024-01-28 12:21:30 -0800

    added script to test oracle insertion

---
+++ experiment/generator/insert.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# This script generates oracles for all bugs in Defects4J for a given TOG.
+current_dir=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
+root_dir=$(dirname "${current_dir}")
+# setup defects4j and sdkman
+export PATH=$PATH:"${DEFECTS4J_HOME}"/framework/bin
+source "${root_dir}/generator/utils/init_sdkman.sh"
+tog=${1}
+
+while IFS=, read -r project_id bug_id _; do
+  # check for a given project or bug id
+  if [ ${#} -gt 1 ]; then
+    if [ "${project_id}" != "${2}" ]; then
+      continue
+    fi
+    if [ ${#} -gt 2 ]; then
+      if [ "${bug_id}" != "${3}" ]; then
+        continue
+      fi
+    fi
+  fi
+  sdk use java "8.0.382-amzn"
+  # checkout Defects4J project and export classpath
+  project_dir="${root_dir}/temp/${project_id}_${bug_id}"
+  defects4j checkout -p "${project_id}" -v "${bug_id}b" -w "${project_dir}"
+  classpath=$(defects4j export -p "cp.compile" -w "${project_dir}")
+  # insert oracles
+  prefix_path="${root_dir}/output/evosuite-prefixes/${project_id}/${bug_id}"
+  oracle_path="${root_dir}/output/${tog}-oracles/${project_id}/${bug_id}"
+  sdk use java "17.0.8-oracle"
+  java -jar "${root_dir}/generator/resources/experiment.jar" "insert_oracles" "${prefix_path}" "${oracle_path}" "${classpath}"
+  rm -r "${root_dir}/temp"
+done < "${current_dir}/resources/modified_classes.csv"
