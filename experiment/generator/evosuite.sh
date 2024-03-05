abe041f99505c8268d43f987b28063285f1ce272
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2024-03-04 19:07:56 -0800

    removed unnecessary evosuite and env script

--- experiment/generator/evosuite.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# This script generates a test suite using EvoSuite and saves the output to
-# "output/evosuite-tests".
-
-# Set and check environment variables
-SCRIPT_DIR="$(cd "$(dirname "${0}")" && pwd -P)"
-# shellcheck source=generator/utils/env.sh
-. "${SCRIPT_DIR}${SEPARATOR}utils${SEPARATOR}env.sh"
