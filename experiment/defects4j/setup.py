64507e9da05d2b9e8e3f09ba6c3802e455df2c2c
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2024-03-04 18:19:52 -0800

    removed defects4j module

--- experiment/defects4j/setup.py
+++
@@ -1,5 +0,0 @@
-"""
-This script checks out a given Defects4J bug in a given project.
-It updates all necessary dependencies and saves project versions to
-"tmp/[projectName]_[bugID]_[buggy]" and "tmp/[projectName]_[bugID]_[fixed]".
-"""
