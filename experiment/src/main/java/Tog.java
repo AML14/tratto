2443a1fe7dacb972d011111808cd387739f08822
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2024-03-14 12:57:44 -0700

    made ExperimentOperationType.java a private enum in Tog

--- experiment/src/main/java/Tog.java
+++ experiment/src/main/java/Tog.java
@@ -1,4 +1,5 @@
-import data.ExperimentOperationType;
+import data.Defects4JOutput;
+import data.OracleOutput;
 import data.TogType;
 
 import java.nio.file.Path;
@@ -28,6 +29,22 @@ import java.nio.file.Path;
  * </ul>
  */
 public class Tog {
+    /**
+     * This enum defines all possible operations of the main Tog class.
+     */
+    private enum ExperimentOperationType {
+        /** Removes oracles from a test suite. */
+        REMOVE_ORACLES,
+        /** Adds oracles to a test prefix. */
+        INSERT_ORACLES,
+        /** Preprocesses an input file to be passed to a TOG. */
+        GENERATE_TOG_INPUT,
+        /** Post-processes the output of a TOG into an {@link OracleOutput}. */
+        GENERATE_ORACLE_OUTPUT,
+        /** Converts Defects4J run_bug_detection output into a {@link Defects4JOutput}. */
+        GENERATE_DEFECTS4J_OUTPUT
+    }
+
     /**
      * Preprocess the input to the necessary format for a given TOG.
      *
