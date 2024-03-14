b8d19e6d4e4de876772bcfa6a7b7cb697be448ef
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2024-03-14 11:49:05 -0700

    updated class-level javadoc

--- experiment/src/main/java/FileUtils.java
+++ experiment/src/main/java/FileUtils.java
@@ -19,9 +19,8 @@ import java.util.stream.Stream;
 
 
 /**
- * This class provides a collection of static methods for a variety of file
- * input and output utilities, such as: creating, copying, moving, writing,
- * reading, etc.
+ * This class provides static methods for file input and output, such as:
+ * creating, copying, moving, writing, reading, etc.
  */
 public class FileUtils {
     /** Private constructor to avoid creating an instance of this class. */
