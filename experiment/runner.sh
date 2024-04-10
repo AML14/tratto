b7cc71f0d2700d8dfbfad8061ddd8439b767d253
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2024-04-10 14:19:53 -0700

    fixed variable names

--- experiment/runner.sh
+++ experiment/runner.sh
@@ -2,8 +2,6 @@
 # This script generates oracles for all bugs in Defects4J for a given TOG.
 current_dir=$(realpath "${BASH_SOURCE[0]}")
 root_dir=$(dirname "${current_dir}")
-# Setup global variables
-source "${root_dir}/generator/utils/global_variables.sh"
 # setup defects4j and sdkman
 export PATH=$PATH:"${DEFECTS4J_HOME}"/framework/bin
 source "${root_dir}/generator/utils/init_sdkman.sh"
@@ -24,7 +22,7 @@ while IFS=, read -r project_id bug_id _; do
       fi
     fi
   fi
-  sdk use java "$JAVA8"
+  sdk use java "8.0.382-amzn"
   # compress test suite
   cd "${root_dir}/output/${tog}-tests/${project_id}/${bug_id}" || exit 1
   tar -cvjSf "${project_id}-${bug_id}b-evosuite.tar.bz2" .
@@ -34,5 +32,5 @@ while IFS=, read -r project_id bug_id _; do
   run_bug_detection.pl -p "${project_id}" -d "${root_dir}/test-suite" -o "${root_dir}/test-suite"
   # cleanup
   rm "${root_dir}/test-suite/${project_id}-${bug_id}b-evosuite.tar.bz2"
-#  sdk use java "17.0.8-oracle"
-done < "${root_dir}/generator/resources/modified_classes.csv"
+  sdk use java "17.0.8-oracle"
+done < "${root_dir}/generator/resources/defects4j/modified_classes.csv"
