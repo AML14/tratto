7e6dc92eaf74a78b70a7d3257dbf37fc2421004e
davide <davide.molinelli@mail.polimi.it>
2024-05-04 21:16:00 +0200

    fixed bug on absolute paths to tratto folders and files, when generating the jar

--- tratto/src/main/java/star/tratto/data/TrattoPath.java
+++ tratto/src/main/java/star/tratto/data/TrattoPath.java
@@ -3,31 +3,34 @@ package star.tratto.data;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-/**
- * An enum to store useful directories and file names used in the dataset
- * generation process.
- */
 public enum TrattoPath {
-    // directories
-    ORACLES_DATASET(Paths.get("src", "main", "resources", "oracles-dataset")),
-    OUTPUT(Paths.get("target", "output")),
-    OUTPUT_DATASET(Paths.get("target", "output", "dataset")),
-    REPOS(Paths.get("src", "main", "resources", "repos")),
-    RESOURCES(Paths.get("src", "main", "resources", "projects-source")),
-    // files
-    ARRAY_METHODS(TrattoPath.REPOS.getPath().resolve("array_methods.json")),
-    INPUT_PROJECTS(TrattoPath.REPOS.getPath().resolve("input_projects.json")),
-    NON_VARIABLE_TOKENS(TrattoPath.REPOS.getPath().resolve("non_variable_tokens.json")),
-    TOKENS_GENERAL_VALUES(TrattoPath.REPOS.getPath().resolve("tokens_general_values.json")),
-    TOKENS_GRAMMAR(TrattoPath.REPOS.getPath().resolve("tokens_grammar.json"));
+    ORACLES_DATASET("oracles-dataset"),
+    OUTPUT("output"),
+    OUTPUT_DATASET("output/dataset"),
+    REPOS("repos"),
+    RESOURCES("projects-source"),
+    ARRAY_METHODS("repos/array_methods.json"),
+    INPUT_PROJECTS("repos/input_projects.json"),
+    NON_VARIABLE_TOKENS("repos/non_variable_tokens.json"),
+    TOKENS_GENERAL_VALUES("repos/tokens_general_values.json"),
+    TOKENS_GRAMMAR("repos/tokens_grammar.json");
 
-    private final Path path;
+    private final String relativePath;
 
-    TrattoPath(Path path) {
-        this.path = path;
+    TrattoPath(String relativePath) {
+        this.relativePath = relativePath;
     }
 
     public Path getPath() {
-        return this.path;
+        return Paths.get(getAbsoluteDirectory(), relativePath);
+    }
+
+    private String getAbsoluteDirectory() {
+        try {
+            return Paths.get(TrattoPath.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getParent().toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ""; // Handle exception properly in your code
+        }
     }
 }
