142ba8eeb5dc636000ebe1dc296de3065d4022c0
davide <davide.molinelli@mail.polimi.it>
2024-05-18 00:20:12 +0200

    improved ground_truth_setup.sh

--- experiment/generator/utils/ground_truth_setup.sh
+++ experiment/generator/utils/ground_truth_setup.sh
@@ -9,13 +9,15 @@ source "${current_dir}/global_variables.sh"
 # Read the CSV file line by line and split into project_id, bug_id, and modified_classes fields
 while IFS=, read -r project_name _ github_link commit_id; do
     cd "$ROOT_DIR"
-    # Clean modified_classes string from undesired white-spaces/line-breaks introduced with the CSV parsing
-    commit_id="${commit_id//[$'\t\r\n ']/}"
-    # Clone project
-    echo "Cloning project: ${project_name}."
-    git clone "${github_link}" "${GROUND_TRUTH_DIR}/${project_name}/project"
-    # Checkout buggy version of the project_id-bug_id project
-    echo "Checkout commit version: project ${project_name}-${commit_id}."
-    cd "${GROUND_TRUTH_DIR}/${project_name}/project"
-    git checkout "${commit_id}"
+    if [ ! -d "${GROUND_TRUTH_DIR}/${project_name}/project" ]; then
+      # Clean modified_classes string from undesired white-spaces/line-breaks introduced with the CSV parsing
+      commit_id="${commit_id//[$'\t\r\n ']/}"
+      # Clone project
+      echo "Cloning project: ${project_name}."
+      git clone "${github_link}" "${GROUND_TRUTH_DIR}/${project_name}/project"
+      # Checkout buggy version of the project_id-bug_id project
+      echo "Checkout commit version: project ${project_name}-${commit_id}."
+      cd "${GROUND_TRUTH_DIR}/${project_name}/project"
+      git checkout "${commit_id}"
+    fi
 done < "$GROUND_TRUTH_PROJECTS"
